2d23222ce502a8a77a9df1527d0adc11d0d952e6
Michael Eichberg <mail@michael-eichberg.de>
2010-07-23 14:08:05 +0200

    we can now parse full ISO Prolog

--- src/compiler/Lexer.pl
+++ src/compiler/Lexer.pl
@@ -29,30 +29,38 @@
 	ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 	POSSIBILITY OF SUCH DAMAGE.
 */
-:- module('Lexer',[tokenize_file/2]).
+
 
 
 
 /**
 	A lexer to tokenize standard (ISO/SAE) Prolog files.
 	<p>
-	The result of tokenizing a file is a list of tokens excluding insignificant
-	white space.<br/>
+	The result of tokenizing a file is a list of tokens representing all
+	parts of the code (including comments).<br/>
 	The types of tokens recognized by this lexer is shown in the following. 
 	<code>LN</code> and <code>CN</code> always identify the line and
 	column where the token was found.
 	<ul>
-	<li>s(S,LN,CN) :- S is a list of char atoms.
+	<li>s(S,LN,CN) :- S is a list of char atoms; i.e., it is a "string".
 	<li>sa(S,LN,CN) :- S is a string atom; <i> the cut ("!") is also an atom.</i></li>
 	<li>i(N,LN,CN) :- N is an integer value.</li>		
 	<li>f(N,LN,CN) :- N is a floating point value.</li>		
 	<li>o(Op,LN,CN) :- Op is a sequence of operator characters (e.g., ':-').</li>
 	<li>o(Op,LN,CN) :- Op is an operator that cannot appear as part of an operator
 	 	sequence (';',',','|').</li>
-	<li>av(V,LN,CN) :- V is the name of an anonymous(a) variable.</li>
-	<li>v(V,LN,CN) :- V is the name of a concrete(c) variable.</li>
+	<li>av(V,LN,CN) :- V is the name of an anonymous variable (av).</li>
+	<li>v(V,LN,CN) :- V is the name of a concrete variable (v).</li>
 	<li>&lt;Special Characters&gt; is every parantheses ("(,),{,},[,]"), special 
-		character is returned as is.</li>			
+		character is returned as is.</li>		
+	</ul>
+	The following tokens are only relevant for a pretty printer / java doc
+	generator.
+	<ul>	
+	<li>sc(Tokens,LN,CN) :- Tokens of a structured comment (/ * * .. * /).</li>
+	<li>eolc(Token,LN,CN) :- Token is an end-of-line comment (% ...).</li>
+	<li>mlc(Token,LN,CN) :- Token is multi-line / inline comment (using / * .. * /)</li>	
+	<li>ws(Token,LN,CN) :- Token is a white space character</li>			
 	</ul>
 	</p>
 	<p>
@@ -82,316 +90,228 @@
 	This is a hand written lexer that delivers sufficient performance for
 	lexing real world (ISO) Prolog programs. The lexer provides information
 	about a token's position to enable subsequent phases to give precise error
-	message.
+	messages. If the lexer does not recognize a special character / symbol, it 
+	prints an error or warning message, but otherwise just ignores it and 
+	continues with the next character.
 	</p>
 	
 	@author Michael Eichberg (mail@michael-eichberg.de)
-	@version 1.0 - July, 22th 2010
+	@version 0.9 - July, 22th 2010 (The lexer works, but it is not fully tested.)
 */
+:- module('Lexer',[tokenize_file/2,tokenize/2]).
 
 
 
+
+/**
+	Tokenizes a file.
+*/
 tokenize_file(File,Tokens) :-
 	open(File,read,Stream),
-	(	% if...
-		peek_char(Stream,C),\+ char_type(C,end_of_file),!, % the stream is not empty
-		tokenize(Stream,1,0,Tokens)
-	;	% else
-		Tokens = []
-	),
+	tokenize(Stream,Tokens),
 	close(Stream).
 
 
 
 /**
-	@signature tokenize(Stream,LN,CN,Tokens)
-	@param LN the current line number (starts with 1)
-	@param CN the number of the current column (starts with 1)
-	@param Tokens is the list of tokens (output argument)
+	Tokenizes a stream of characters.
+	
+	@signature tokenize(Stream,Tokens)
+	@arg(in) Stream a stream that supports reading characters
+	@arg(out) Tokens the list of recognized tokens
 */
-tokenize(Stream,LN,CN,Tokens) :-
+tokenize(Stream,[]) :- at_end_of_stream(Stream),!.
+tokenize(Stream,Tokens) :-
 	get_char(Stream,C),
-	ICN is CN + 1,
-	token(C,LN,ICN,Stream,Token,NLN,NCN),
+	read_token(C,Stream,Token),
 	(	% if...
 		Token = none,!,
 		Tokens = Ts
 	;	% else...
 		Tokens = [Token|Ts]
 	),
-	(
-		at_end_of_stream(Stream),!,
-		Ts = []
-	;
-		tokenize(Stream,NLN,NCN,Ts)
-	).
+	tokenize(Stream,Ts).
+
 
 
 
 /**
-	Based on Char and at most one further character (using peek_char), the type
-	of the token is determined. Reading the rest of the chars of the token
-	is delegated to the corresponding "read_*" predicates.
+	Reads in a token of a specific type.</br >
+	Based on the previously read character Char and at most one further character 
+	(using peek_char), the type of the token is determined and reading the rest 
+	of the token's chars is delegated to the corresponding "read_*" predicates.
 	
-	@signature token(Char,LN,CN,Stream,Token,NLN,NCN)
-	@param Char the last read token, based on which the type of the token will 
-		be decided.
-	@param LN the current line number (starts with 1)
-	@param CN the number of the current column (starts with 1)
-	@param Token the read token (either an atom or a compound term; token is
-		unified with 'none' in case of insignificant white space.
+	@signature read_token(Char,Stream,Token)
+	@param Char the last read token. This character is used to identify the 
+		type of the current token and determines how the following characters
+		are interpreted.
+	@param Token the read token. An atom in case of a special character (e.g., all 
+		forms of parantheses); 'none' if an 
+		errror is detected; a compound term describing the token and its position
+		in all other cases.)
 */
+read_token('%',Stream,eolc(Token,LN,CN)) :- 
+	!,
+	stream_position(Stream,LN,CN),
+	read_eol_comment(Stream,Cs),
+	atom_chars(Token,Cs).
 
-token('%',LN,_CN,Stream,none,NLN,0) :- !,
-	read_eol_comment(Stream),NLN is LN +1.
+read_token('!',Stream,sa('!',LN,CN)) :- !, stream_position(Stream,LN,CN).
 
 % "and (,)", "or (;)", etc. are special operators because they cannot
 % contribute to an operator name (e.g. ':-')
-token(',',LN,CN,_Stream,o(',',LN,CN),LN,CN) :- !.	
-token(';',LN,CN,_Stream,o(';',LN,CN),LN,CN) :- !.	
-token('|',LN,CN,_Stream,o('|',LN,CN),LN,CN) :- !.	
+read_token(',',Stream,o(',',LN,CN)) :- !, stream_position(Stream,LN,CN).	
+read_token(';',Stream,o(';',LN,CN)) :- !, stream_position(Stream,LN,CN).	
+read_token('|',Stream,o('|',LN,CN)) :- !, stream_position(Stream,LN,CN).	
 
 % parantheses are "returned" as is 
-token('(',LN,CN,_Stream,'(',LN,CN) :- !.	
-token(')',LN,CN,_Stream,')',LN,CN) :- !.	
-token('{',LN,CN,_Stream,'{',LN,CN) :- !.	
-token('}',LN,CN,_Stream,'}',LN,CN) :- !.	
-token('[',LN,CN,_Stream,'[',LN,CN) :- !.	
-token(']',LN,CN,_Stream,']',LN,CN) :- !.	
+read_token('(',_Stream,'(') :- !.	
+read_token(')',_Stream,')') :- !.	
+read_token('{',_Stream,'{') :- !.	
+read_token('}',_Stream,'}') :- !.	
+read_token('[',_Stream,'[') :- !.	
+read_token(']',_Stream,']') :- !.	
 
 % an anonymous variable
-token('_',LN,CN,Stream,Token,LN,NCN) :- !,
-	read_identifier(Stream,X,CN,NCN),
-	ID = ['_'|X],
-	atom_chars(AID,ID),
-	Token = av(AID,LN,CN).
+read_token('_',Stream,av(AID,LN,CN)) :- 
+	!,
+	stream_position(Stream,LN,CN),
+	read_identifier(Stream,Cs),
+	ID = ['_'|Cs],
+	atom_chars(AID,ID).
 
 % a quoted atom
-token('\'',LN,CN,Stream,Token,NLN,NCN) :- 
-	read_string(Stream,'\'',X,LN,CN,NLN,NCN),
-	atom_chars(AID,X),
-	Token = sa(AID,LN,CN).
+read_token('\'',Stream,sa(AID,LN,CN)) :- 
+	!,
+	stream_position(Stream,LN,CN),
+	read_string(Stream,'\'',Cs),
+	atom_chars(AID,Cs).
 	
 % a string	
-token('"',LN,CN,Stream,s(X,LN,CN),NLN,NCN) :- 
-	read_string(Stream,'"',X,LN,CN,NLN,NCN).
+read_token('"',Stream,s(S,LN,CN)) :- 
+	!,
+	stream_position(Stream,LN,CN),
+	read_string(Stream,'"',S).
 
 % white space
-token(W,LN,CN,_Stream,none,NLN,NCN) :- 
-	char_type(W,space),!,
-	(
-		W = '\n',!,
-		NLN is LN + 1, NCN = 0
-	;
-		NLN = LN, NCN = CN
-	).
+read_token(W,_Stream,ws(W)) :- char_type(W,space), !.
 
 % a string atom
-token(C,LN,CN,Stream,Token,LN,NCN) :- 
-	char_type(C,lower),!,
-	read_identifier(Stream,X,CN,NCN),
-	ID = [C|X],
-	atom_chars(AID,ID),
-	Token = sa(AID,LN,CN).
-token('!',LN,CN,_Stream,sa('!',LN,CN),LN,CN) :- !.	
+read_token(C,Stream,sa(AID,LN,CN)) :- 
+	char_type(C,lower),
+	!,
+	stream_position(Stream,LN,CN),
+	read_identifier(Stream,Cs),
+	ID = [C|Cs],
+	atom_chars(AID,ID).
 
 % a variable name
-token(C,LN,CN,Stream,Token,LN,NCN) :- 
-	char_type(C,upper),!,
-	read_identifier(Stream,X,CN,NCN),
-	ID = [C|X],
-	atom_chars(AID,ID),
-	Token = v(AID,LN,CN).	
+read_token(C,Stream,v(AID,LN,CN)) :- 
+	char_type(C,upper),
+	!,
+	stream_position(Stream,LN,CN),
+	read_identifier(Stream,Cs),
+	ID = [C|Cs],
+	atom_chars(AID,ID).	
 
 % a number
-token(I,LN,CN,Stream,Token,LN,NCN) :-
-	char_type(I,digit),!,
-	read_number(Stream,X,CN,NCN),
-	NCs = [I|X],
-	number_chars(N,NCs),
-	Token = n(N,LN,CN).
+read_token(I,Stream,n(N,LN,CN)) :-
+	char_type(I,digit),
+	!,
+	stream_position(Stream,LN,CN),
+	read_number(Stream,Is),
+	NCs = [I|Is],
+	number_chars(N,NCs).
 
-% a multi-line comments	
-token('/',LN,CN,Stream,Token,NLN,NCN) :- 
-	peek_char(Stream,'*'),!, 
+% a multi-line comment; it is either a structured or an unstructured comment
+read_token('/',Stream,Token) :- 
+	peek_char(Stream,'*'),
+	!, 
 	get_char(Stream,_), % /*/ is not considered to be a valid comment...
-	ICN is CN + 1,
-	read_ml_comment(Stream,LN,ICN,Token,NLN,NCN).
+	read_ml_comment(Stream,Token).
 
 % a sequence of operator characters 
 % (This clause has to come AFTER handling of multi-line comments!)
-token(C,LN,CN,Stream,Token,LN,NCN) :- 
-	operator_char(C),!,
-	read_operators(Stream,X,CN,NCN),
-	ID = [C|X],
-	atom_chars(AID,ID),
-	Token = o(AID,LN,CN).
+read_token(Op,Stream,o(AOPs,LN,CN)) :- 
+	operator_char(Op),
+	!,
+	stream_position(Stream,LN,CN),
+	read_operators(Stream,OPs),
+	O = [Op|OPs],
+	atom_chars(AOPs,O).
 	
 % fallback case to enable the lexer to continue if an error is found...
-token(C,LN,CN,Stream,none,LN,CN) :-
-	current_stream(File,read,Stream),
-	atomic_list_concat(['ERROR:',File,':',LN,':',CN,': unrecognized symbol (',C,')\n'],MSG),
-	write(MSG).
+read_token(C,Stream,none) :- lexer_error(Stream,['unrecognized symbol (',C,')']).
 
 
 
-% an identifier is a squence of upper or lower case letters, digits and "_".
-read_identifier(Stream,[C|X],CN,NCN) :-
+
+% an identifier is a sequence of upper or lower case letters, digits and "_".
+read_identifier(Stream,[]) :- at_end_of_stream(Stream),!.
+read_identifier(Stream,[C|Cs]) :-
 	peek_char(Stream,C),
-	( 
+	( 	% if...
 		char_type(C,alnum) 
-	; 
+	;	% or...
 		C = '_' 
-	),!,
-	get_char(Stream,_),
-	ICN is CN + 1,
-	read_identifier(Stream,X,ICN,NCN).	
-read_identifier(_Stream,[],CN,CN).	
-
-
-
-read_string(Stream,Delimiter,R,LN,CN,NLN,NCN) :-
-	get_char(Stream,C),!,
-	(	% if C is a newline character ...
-		char_type(C,newline),!,
-		ILN is LN + 1,	ICN = 0
-	;	% C is not a newline character ...
-		ILN = LN, ICN is CN + 1
 	),
+	!, % then ...
+	get_char(Stream,_),
+	read_identifier(Stream,Cs).	
+read_identifier(_Stream,[]).	
+
+
+
+read_string(Stream,Delimiter,[]) :- 
+	at_end_of_stream(Stream,['missing delimiter: ',Delimiter]),
+	!.
+read_string(Stream,Delimiter,R) :-
+	get_char(Stream,C),!,
 	(	% C is the delimiter...
 		C = Delimiter,!,
-		NLN = ILN, NCN = ICN, R = []
+		R = []
 	;	% C is the start of an escape sequence...
 		C = '\\',!,
 		(	% but we are at the end of the stream...
-			at_end_of_stream(Stream),!,
-			current_stream(File,read,Stream),
-			atomic_list_concat(['ERROR:',File,': unexpected end of file while parsing escape sequence (\\)'],MSG),
-			write(MSG),
-			NLN = ILN, NCN = ICN, R = []
+			at_end_of_stream(Stream,['unfinished escape sequence (\\)']),
+			!,
+			R = []
 		;	
 			get_char(Stream,NC),
 			(
 				NC = '\\',!,
-				R = ['\\'|X]
+				R = ['\\'|Cs]
 			;
 				NC = 't',!,
-				R = ['\t'|X]
+				R = ['\t'|Cs]
 			;
 				NC = 'n',!,
-				R = ['\n'|X]
+				R = ['\n'|Cs]
 			;
 				NC = '\'',!,
-				R = ['\''|X]
+				R = ['\''|Cs]
 			;
-				current_stream(File,read,Stream),
-				atomic_list_concat(['WARNING:',File,':',LN,':',CN,': unsupported escape character (',NC,'); escape symbol (\\) ignored'],MSG),
-				write(MSG),
-				R = [NC|X]
+				lexer_error(Stream,['unsupported escape sequence (\\',NC,')']),
+				R = [NC|Cs]
 			),
-			IILN is LN + 1,IICN is CN +1,
-			read_string(Stream,Delimiter,X,IILN,IICN,NLN,NCN)
+			read_string(Stream,Delimiter,Cs)
 		)
 	;	% C is an "ordinary" character...
-		(
-			at_end_of_stream(Stream),!,
-			current_stream(File,read,Stream),
-			atomic_list_concat(['ERROR:',File,': unexpected end of file while parsing (a closing \' is missing)'],MSG),
-			write(MSG),
-			NLN = ILN,
-			NCN = ICN,
-			R = []
-		;	% the base case
-			R = [C|X],
-			read_string(Stream,Delimiter,X,ILN,ICN,NLN,NCN)
-		)
+		R = [C|Cs],
+		read_string(Stream,Delimiter,Cs)
 	).
-read_string(_Stream,_Delimiter,[],LN,CN,LN,CN).
+
 
 
 
 % a squence of "operator" signs
-read_operators(Stream,[C|X],CN,NCN) :-
+read_operators(Stream,[]) :- at_end_of_stream(Stream),!.
+read_operators(Stream,[C|Cs]) :-
 	peek_char(Stream,C),
 	operator_char(C),!,
 	get_char(Stream,_),
-	ICN is CN + 1,
-	read_operators(Stream,X,ICN,NCN).	
-read_operators(_Stream,[],CN,CN).
-
-
-
-% an EOL comment can contain arbitrary chars and extends until the newline character 
-read_eol_comment(Stream) :-
-	get_char(Stream,C),
-	(
-		C \= '\n',!,
-		\+ at_end_of_stream(Stream),
-		read_eol_comment(Stream)
-	;
-		true % C = '\n'
-	).
-
-
-% an integer or floating point value
-read_number(Stream,X,CN,NCN) :-
-	read_int(Stream,SX,SZ,CN,NCN),
-	
-	SZ = [],X=SX.
-	
-	
-
-% reads the characters of an integer value using a difference list
-read_int(Stream,[C|SY],SZ,CN,NCN) :-
-	peek_char(Stream,C),
-	char_type(C,digit),
-	!,
-	get_char(Stream,_),
-	ICN is CN + 1,
-	read_int(Stream,SY,SZ,ICN,NCN).
-read_int(_Stream,SZ,SZ,CN,CN).
-
-	
-
-% a multi-line comment extends until "*/" is found
-read_ml_comment(Stream,LN,CN,Token,NLN,NCN) :-
-	peek_char(Stream,C),
-	(
-		C = '*',!,
-		get_char(Stream,_),
-		ICN is CN + 1,
-		read_structured_ml_comment(Stream,LN,ICN,Token,NLN,NCN)
-	;
-		Token = none,
-		read_unstructured_ml_comment(Stream,LN,CN,NLN,NCN)
-	).
-	
-read_structured_ml_comment(Stream,LN,CN,none,NLN,NCN) :-
-	read_unstructured_ml_comment(Stream,LN,CN,NLN,NCN). % TODO Implement lexing of structured comments...
-	
-read_unstructured_ml_comment(Stream,LN,CN,NLN,NCN) :-
-	get_char(Stream,C),
-	(
-		C = '*',!,
-		ILN = LN,
-		ICN is CN + 1,
-		(
-			peek_char(Stream,NC),NC='/',!,
-			get_char(Stream,_),
-			NLN is ILN, NCN is ICN + 1
-		;
-			read_unstructured_ml_comment(Stream,ILN,ICN,NLN,NCN)				
-		)
-	;
-		( 	
-			C = '\n',!,
-			ILN is LN +1, ICN = 0 
-		; 	
-			ILN = LN, ICN is CN +1 
-		),
-		read_unstructured_ml_comment(Stream,ILN,ICN,NLN,NCN)
-	).
-
+	read_operators(Stream,Cs).	
+read_operators(_Stream,[]).
 
 
 % the list of all operators that are allowed to be combined to form new
@@ -413,3 +333,188 @@ operator_char('@').
 operator_char('#').
 operator_char('$').
 operator_char('&').
+
+
+
+
+% an EOL comment can contain arbitrary chars and extends until the end of the line
+read_eol_comment(Stream,[]) :- at_end_of_stream(Stream),!.
+read_eol_comment(Stream,Cs) :-
+	get_char(Stream,C),
+	process_eol_comment_char(Stream,C,Cs).
+process_eol_comment_char(_Stream,'\n',[]) :- !.
+process_eol_comment_char(Stream,C,[C|Cs]) :- read_eol_comment(Stream,Cs),!.
+
+
+
+% an integer or floating point value
+read_number(Stream,S1) :-
+	read_int_part(Stream,S1,S2),
+	read_fp_part(Stream,S2,[]).
+	
+	
+read_fp_part(Stream,[],[]) :- at_end_of_stream(Stream),!.	
+read_fp_part(Stream,S1,SZ) :-
+	peek_char(Stream,C),
+	read_fp_part1(C,Stream,S1,SZ).
+	
+read_fp_part1(C,_Stream,[],[]) :- char_type(C,space),!.	
+read_fp_part1('.',Stream,S1,SZ) :-
+	!,
+	stream_property(Stream,position(BeforeDot)),
+	get_char(Stream,_),
+	read_int_part(Stream,S1,S2),
+	(														% if ...
+		S1 = [], 										% no floating point segment
+		!,													% is found
+		set_stream_position(Stream,BeforeDot)  % then the "." is an operator
+	;	% succeeded reading floating point segment
+		(
+			at_end_of_stream(Stream),
+			!,
+			S2 = [],
+			SZ = []
+		;
+			peek_char(Stream,C),
+			read_fp_part2(C,Stream,S2,SZ)
+		)
+	).
+read_fp_part1(C,Stream,S1,SZ) :-  read_fp_part2(C,Stream,S1,SZ).
+
+read_fp_part2(C,_Stream,[],[]) :- char_type(C,space),!.
+read_fp_part2('e',Stream,S1,SZ) :- 
+	!,
+	get_char(Stream,_),
+	read_exponent(Stream,S1,SZ).
+read_fp_part2(C,Stream,[],[]) :-
+	char_type(C,alpha),
+	lexer_error(Stream,['unexpected symbol (',C,')']).
+read_fp_part2(_C,_Stream,SZ,SZ). 
+
+read_exponent(Stream,[],[]) :- at_end_of_stream(Stream,['exponent expected']),!.
+read_exponent(Stream,S1,SZ) :-
+	get_char(Stream,C),
+	(
+		C = '-',
+		!,
+		S1 = [C|SX]
+	;
+		S1 = SX
+	),
+	read_int_part(Stream,SX,SZ).
+	
+
+
+% reads the characters of an integer value using a difference list
+read_int_part(Stream,SZ,SZ) :- at_end_of_stream(Stream),!.
+read_int_part(Stream,[C|SY],SZ) :-
+	peek_char(Stream,C),
+	char_type(C,digit),
+	!,
+	get_char(Stream,_),
+	read_int_part(Stream,SY,SZ).
+read_int_part(_Stream,SZ,SZ).
+
+	
+	
+
+% a multi-line comment extends until "*/" is found
+read_ml_comment(Stream,_) :- at_end_of_stream(Stream,['expected */']),!.
+read_ml_comment(Stream,Token) :-
+	peek_char(Stream,C),
+	(
+		C = '*',!,
+		get_char(Stream,_),
+		read_unstructured_ml_comment(Stream,Cs)
+	;
+		stream_position(Stream,LN,CN),
+		read_unstructured_ml_comment(Stream,Cs),
+		atom_chars(ACs,Cs),
+		Token=mlc(ACs,LN,CN)
+	).
+	
+
+read_unstructured_ml_comment(Stream,[]) :- 
+	at_end_of_stream(Stream,['expected */']),
+	!.
+read_unstructured_ml_comment(Stream,R) :-
+	get_char(Stream,C),
+	(
+		C = '*',!,
+		(
+			peek_char(Stream,NC),NC='/',!,
+			get_char(Stream,_),
+			R = []
+		;
+			R = [C|Cs],
+			read_unstructured_ml_comment(Stream,Cs)				
+		)
+	;
+		R = [C|Cs],
+		read_unstructured_ml_comment(Stream,Cs)
+	).
+
+
+read_structured_ml_comment(Stream,Token) :-
+	read_unstructured_ml_comment(Stream,Token).
+/*
+read_structured_ml_comment(Stream,Token) :-
+	line_count(Stream,LN),line_position(Stream,CN),
+	tokenize_sc(Stream,Tokens),
+	Token = sc(Tokens,LN,CN).
+
+
+tokenize_sc(Stream,[]) :- 
+	at_end_of_stream(Stream),!,
+	current_stream(File,read,Stream),
+	atomic_list_concat(['ERROR:',File,': unexpected end of file while parsing structured comment'],MSG),
+	write(MSG).
+tokenize_sc(Stream,Tokens) :-
+	get_char(Stream,C),
+	( 
+		C = '*', peek_char(Stream,'/'), !, get_char(Stream,_), Tokens = []
+	;
+		read_token_sc(C,Stream,Token),
+		(	% if...
+			Token = none,!,
+			Tokens = Ts
+		;	% else...
+			Tokens = [Token|Ts]
+		),
+		tokenize_sc(Stream,Ts)
+	).
+	
+
+read_token_sc('@',Stream,'@'(T,LN,CN)) :- 
+	!,
+	line_count(Stream,LN),line_position(Stream,CN),
+	read_identifier(Stream,T).
+% TODO read_token_sc('{',Stream,Token) :- ...
+
+read_token_sc(C,Stream,tf(TF,LN,CN)) :- 
+	!,
+	line_count(Stream,LN),
+	line_position(Stream,CN),
+	*/
+
+
+at_end_of_stream(Stream,ErrorMessage) :-
+	at_end_of_stream(Stream),
+	!,
+	atomic_list_concat(ErrorMessage,EM),
+	lexer_error(Stream,['unexpected end of file (',EM,')']).
+
+	
+stream_position(Stream,LN,CN) :-
+	line_count(Stream,LN),
+	line_position(Stream,CN).
+
+
+lexer_error(Stream,ErrorMessage) :-
+	atomic_list_concat(ErrorMessage,EM),
+	stream_position(Stream,LN,CN),
+	current_stream(File,read,Stream),
+	atomic_list_concat(['ERROR:',File,':',LN,':',CN,': ',EM,'\n'],MSG),
+	write(MSG).
+	
+	
