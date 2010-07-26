c5fc8d0cd5a633ff8e0f31f1e27d1d862b6e1e31
Michael Eichberg <mail@michael-eichberg.de>
2010-07-26 23:58:24 +0200

    added some comments; we are now only lacking support for lists

--- src/compiler/Parser.pl
+++ src/compiler/Parser.pl
@@ -1,3 +1,35 @@
+/* License (BSD Style License):
+   Copyright (c) 2010
+   Department of Computer Science
+   Technische Universität Darmstadt
+   All rights reserved.
+
+   Redistribution and use in source and binary forms, with or without
+   modification, are permitted provided that the following conditions are met:
+
+    - Redistributions of source code must retain the above copyright notice,
+      this list of conditions and the following disclaimer.
+    - Redistributions in binary form must reproduce the above copyright notice,
+      this list of conditions and the following disclaimer in the documentation
+      and/or other materials provided with the distribution.
+    - Neither the name of the Software Technology Group or Technische 
+      Universität Darmstadt nor the names of its contributors may be used to 
+      endorse or promote products derived from this software without specific 
+      prior written permission.
+
+   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+   AND ANY EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+   IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+   ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+   LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+   CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+   SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+   INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+   CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+   ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+   POSSIBILITY OF SUCH DAMAGE.
+*/
+
 
 /*
 	In general, to parse prolog terms we would like to define a rule 
@@ -24,7 +56,7 @@ program(Ops,[S]) --> stmt(Ops,S).
 
 % ... add code to check that the term is valid top-level term.... i.e., 
 % it is an atom, a compound term, a directive or a clause definition
-stmt(Ops,T) --> term(Ops,T),[o('.',_LN,_CN)]. 
+stmt(Ops,T) --> term(Ops,T),[o('.',_LN,_CN)],{write(T),nl,!}. 
 
 
 
@@ -65,9 +97,9 @@ atom(sa(A,LN,CN)) --> [sa(A,LN,CN)]. % handles vars, string atoms and integer at
 atom(i(I,LN,CN)) --> [i(I,LN,CN)].
 
 
+
 var(v(V,LN,CN)) --> [v(V,LN,CN)].
 var(av(V,LN,CN)) --> [av(V,LN,CN)].
-
 
 
 
@@ -99,6 +131,7 @@ is_prefix(Op,Ops) :-
 		Mode = fx;
 		Mode = fy
 	),	write('is_prefix: '),write(Op),nl.
+
 
 
 default_op_table([
