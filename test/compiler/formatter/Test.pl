03780d0bd6da6318b9a5e4e6dc9d83550c21081c
Michael Eichberg <mail@michael-eichberg.de>
2010-12-06 12:19:06 +0100

    refactoring of test.pl

--- test/compiler/formatter/Test.pl
+++ test/compiler/formatter/Test.pl
@@ -7,13 +7,12 @@
 :- ensure_loaded('src/compiler/Formatter.pl').
 
 do_formatting(FileName,Solution,Options) :-
-        atomic_list_concat(['test/compiler/formatter/data/',FileName],File),
-        atomic_list_concat(['test/compiler/formatter/data/',Solution],FileSolution),
+   atomic_list_concat(['test/compiler/formatter/data/',FileName],File),
+   atomic_list_concat(['test/compiler/formatter/data/',Solution],FileSolution),
         
-        tokenize_file(File,Ts,Options), clauses(Ts,Cs), format_file(Cs,'',Out), nl,write('###FORMATTED###\n'),write(Out),
-        
-        load_solution(FileSolution,Lines),
-        Out = Lines.
+	load_solution(FileSolution,Solution),
+	tokenize_file(File,Ts,Options), clauses(Ts,Cs), format_file(Cs,'',Formatted), nl,write('###FORMATTED###\n'),write(Formatted),
+	Formatted = Solution.
 
 load_solution(Solution,Lines) :-
    open(Solution,read,Stream),
@@ -22,20 +21,22 @@ load_solution(Solution,Lines) :-
    nl,write('###SOLUTION###\n'),write(Lines),
    close(Stream).
 
-readLines(Stream,[]):-
-   at_end_of_stream(Stream).
+
 
 readLines(Stream,[X|T]):-
-   \+ at_end_of_stream(Stream),
+   \+ at_end_of_stream(Stream),!,
    get_char(Stream,X),
    readLines(Stream,T).
+readLines(_Stream,[]).
+
+
 
 :- begin_tests(formatter).
 
- test(emptyFormatter) :- do_formatting('EmptyTest.pl','EmptyTest_formatted.pl',[]).
+ test(emptyFormatter) :- do_formatting('EmptyTest.pl','EmptyTest_formatted.pl',[]),!.
 
- test(whitespaces1) :- do_formatting('Whitespace1.pl','Whitespace1_formatted.pl',[]).
+ test(whitespaces1) :- do_formatting('Whitespace1.pl','Whitespace1_formatted.pl',[]),!.
 
- test(whitespaces2) :- do_formatting('Whitespace2.pl','Whitespace2_formatted.pl',[]).
+ test(whitespaces2) :- do_formatting('Whitespace2.pl','Whitespace2_formatted.pl',[]),!.
 
  :- end_tests(formatter).
