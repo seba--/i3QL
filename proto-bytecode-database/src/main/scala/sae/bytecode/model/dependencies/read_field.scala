c4780bcf20dda6b9de2aa54a5c3ebbb95c12ea0a
Malte <me@local>
2011-06-20 16:43:15 +0200

    Merge branch 'master' of ssh://github.com/Delors/SAE
    Conflicts:
    	proto-bytecode-database/src/test/scala/sae/test/BytecodeChangeEventTest.scala

--- proto-bytecode-database/src/main/scala/sae/bytecode/model/dependencies/read_field.scala
+++ proto-bytecode-database/src/main/scala/sae/bytecode/model/dependencies/read_field.scala
@@ -9,7 +9,7 @@ import sae.bytecode.model.{Field, Method}
  *
  */
 
-class read_field (val source: Method, val target: Field)
+case class read_field (val source: Method, val target: Field)
         extends Dependency[Method, Field] {
 
 }
