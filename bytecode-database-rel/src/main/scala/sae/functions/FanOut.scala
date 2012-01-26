e4443dd560f59f2ccefbe51ad119e8752341f45c
Ralf Mitschke <mitschke@st.informatik.tu-darmstadt.de>
2012-01-26 18:44:39 +0100

    changed version to 0.4.0

--- bytecode-database-rel/src/main/scala/sae/functions/FanOut.scala
+++ bytecode-database-rel/src/main/scala/sae/functions/FanOut.scala
@@ -6,32 +6,33 @@ import scala.collection.JavaConversions._
 import de.tud.cs.st.bat._
 import scala.collection.mutable.Set
 
-
-private class FanOutIntern[Domain <: AnyRef](val f : Domain => (Seq[Type], Type), val select : Type => Boolean) extends SelfMaintainalbeAggregateFunction[Domain, Set[String]] {
+private class FanOutIntern[Domain <: AnyRef](val f: Domain => (Seq[Type], Type),
+                                             val select: Type => Boolean) extends SelfMaintainableAggregateFunction[Domain, Set[String]]
+{
 
     import com.google.common.collect._;
     val dep = HashMultiset.create[String]()
 
-    var value : Option[Domain] = None
-    def add(d : Domain) = {
+    var value: Option[Domain] = None
+
+    def add(d: Domain) = {
         if (select(f(d)._2))
             dep.add(f(d)._2.toJava)
         f(d)._1.foreach(x => {
-            val t = x.toJava
-            if (select(x)) 
-            dep.add(x.toJava)
-            })
-            dep.elementSet().clone()
+            if (select(x))
+                dep.add(x.toJava)
+        })
+        dep.elementSet().clone()
     }
-    
-    def remove(d : Domain) = {
+
+    def remove(d: Domain) = {
         if (select(f(d)._2))
             dep.remove(f(d)._2.toJava)
         f(d)._1.foreach(x => if (select(x)) dep.remove(x.toJava))
         dep.elementSet().clone()
     }
-    
-    def update(oldV : Domain, newV : Domain) = {
+
+    def update(oldV: Domain, newV: Domain) = {
         if (select(f(oldV)._2))
             dep.remove(f(oldV)._2.toJava)
         f(oldV)._1.foreach(x => if (select(x)) dep.remove(x.toJava))
@@ -42,10 +43,12 @@ private class FanOutIntern[Domain <: AnyRef](val f : Domain => (Seq[Type], Type)
     }
 }
 
-object FanOut {
-    def apply[Domain <: AnyRef](f : (Domain => (Seq[Type], Type)), select : Type => Boolean) = {
-        new SelfMaintainalbeAggregateFunctionFactory[Domain, Set[String]] {
-            def apply() : SelfMaintainalbeAggregateFunction[Domain, Set[String]] = {
+object FanOut
+{
+    def apply[Domain <: AnyRef](f: (Domain => (Seq[Type], Type)), select: Type => Boolean) = {
+        new SelfMaintainableAggregateFunctionFactory[Domain, Set[String]]
+        {
+            def apply(): SelfMaintainableAggregateFunction[Domain, Set[String]] = {
                 new FanOutIntern[Domain](f, select)
             }
         }
