b2334aae7a1ccc013acdb1721b1b3c290d38eedd
Ralf Mitschke <mitschke@st.informatik.tu-darmstadt.de>
2012-02-02 13:10:46 +0100

    added adapters for field/method reference

---
+++ unisson/unisson-query-code_model/src/main/scala/unisson/query/code_model/FieldReferenceAdapter.scala
@@ -0,0 +1,40 @@
+package unisson.query.code_model
+
+import de.tud.cs.st.vespucci.interfaces.IFieldDeclaration
+import sae.bytecode.model.FieldReference
+
+/**
+ *
+ * Author: Ralf Mitschke
+ * Date: 12.12.11
+ * Time: 12:48
+ *
+ */
+class FieldReferenceAdapter(val element: FieldReference)
+        extends IFieldDeclaration with SourceElement[FieldReference]
+{
+    def getPackageIdentifier = element.declaringClass.packageName
+
+    def getSimpleClassName = element.declaringClass.simpleName
+
+    def getLineNumber = -1
+
+    override def hashCode() = element.hashCode()
+
+    override def equals(obj: Any): Boolean = {
+        if (!obj.isInstanceOf[FieldReferenceAdapter]) {
+            return false
+        }
+        element.equals(obj.asInstanceOf[FieldReferenceAdapter].element)
+    }
+
+
+    def getFieldName = element.name
+
+    def getTypeQualifier = element.fieldType.signature
+
+    override def toString = element.declaringClass.signature +
+            element.name +
+            ":" + element.fieldType.signature
+
+}
--- unisson/unisson-query-code_model/src/main/scala/unisson/query/code_model/SourceElement.scala
+++ unisson/unisson-query-code_model/src/main/scala/unisson/query/code_model/SourceElement.scala
@@ -2,7 +2,7 @@ package unisson.query.code_model
 
 import de.tud.cs.st.vespucci.interfaces.ICodeElement
 import de.tud.cs.st.bat.{ArrayType, ObjectType}
-import sae.bytecode.model.{FieldDeclaration, MethodDeclaration, FieldReference}
+import sae.bytecode.model.{MethodReference, FieldDeclaration, MethodDeclaration, FieldReference}
 
 /**
  *
@@ -26,8 +26,14 @@ object SourceElement
         if (element.isInstanceOf[MethodDeclaration]) {
             return new MethodDeclarationAdapter(element.asInstanceOf[MethodDeclaration])
         }
+        if (element.isInstanceOf[MethodReference]) {
+            return new MethodReferenceAdapter(element.asInstanceOf[MethodReference])
+        }
         if (element.isInstanceOf[FieldDeclaration]) {
             return new FieldDeclarationAdapter(element.asInstanceOf[FieldDeclaration])
+        }
+        if (element.isInstanceOf[FieldReference]) {
+            return new FieldReferenceAdapter(element.asInstanceOf[FieldReference])
         }
         if (element.isInstanceOf[ArrayType]) {
             return new ArrayDeclaration(element.asInstanceOf[ArrayType])
