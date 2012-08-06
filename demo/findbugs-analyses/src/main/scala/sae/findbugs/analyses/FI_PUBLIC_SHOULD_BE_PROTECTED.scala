20456c8cbab296c4d24433ae5328b280caafcdc0
Ralf Mitschke <mitschke@st.informatik.tu-darmstadt.de>
2012-08-06 15:19:19 +0200

    added info on additional FindBugs Analyses

--- demo/findbugs-analyses/src/main/scala/sae/findbugs/analyses/FI_PUBLIC_SHOULD_BE_PROTECTED.scala
+++ demo/findbugs-analyses/src/main/scala/sae/findbugs/analyses/FI_PUBLIC_SHOULD_BE_PROTECTED.scala
@@ -4,7 +4,7 @@ import sae.bytecode.Database
 import sae.LazyView
 import sae.bytecode.model.MethodDeclaration
 import de.tud.cs.st.bat.{ReferenceType, VoidType}
-import sae.syntax.RelationalAlgebraSyntax._
+
 
 /**
  *
@@ -12,12 +12,13 @@ import sae.syntax.RelationalAlgebraSyntax._
  * Date: 27.01.12
  * Time: 19:27
  *
- *  FINDBUGS: FI: Finalizer should be protected, not public (FI_PUBLIC_SHOULD_BE_PROTECTED)
+ * FINDBUGS: FI: Finalizer should be protected, not public (FI_PUBLIC_SHOULD_BE_PROTECTED)
  */
 object FI_PUBLIC_SHOULD_BE_PROTECTED
 {
 
-    def apply(database: Database): LazyView[ReferenceType] = {
+    def asOperators(database: Database): LazyView[ReferenceType] = {
+        import sae.syntax.RelationalAlgebraSyntax._
         Π((_: MethodDeclaration).declaringRef)(
             σ(
                 (m: MethodDeclaration) =>
@@ -26,4 +27,16 @@ object FI_PUBLIC_SHOULD_BE_PROTECTED
         )
     }
 
+
+    def apply(database: Database): LazyView[ReferenceType] = {
+        import sae.syntax.sql._
+        import database._
+        //val a = SELECT(*) FROM declared_methods WHERE ((m: MethodDeclaration) => (m.name == "finalize" && m.isPublic && m.returnType == VoidType() && m.parameters.isEmpty))
+
+        SELECT (declaringType) FROM declared_methods WHERE
+                (_.name == "finalize") AND
+                (_.isPublic) AND
+                (_.returnType == void) AND
+                (_.parameters.isEmpty)
+    }
 }
