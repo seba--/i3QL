bb08548a869435bb2267df5cb029f0ca617e4cdf
Ralf Mitschke <mitschke@st.informatik.tu-darmstadt.de>
2012-08-11 18:37:23 +0200

    Tweaked selection on two relations and implemented a join operator

---
+++ core/src/main/scala/sae/syntax/sql/impl/Projection2.scala
@@ -0,0 +1,19 @@
+package sae.syntax.sql.impl
+
+import sae.LazyView
+import sae.syntax.sql.SELECT_CLAUSE_2
+
+/**
+ *
+ * Author: Ralf Mitschke
+ * Date: 03.08.12
+ * Time: 19:55
+ *
+ */
+case class Projection2[DomainA <: AnyRef, DomainB <: AnyRef, Range <: AnyRef](projection: (DomainA, DomainB) => Range, distinct: Boolean)
+    extends SELECT_CLAUSE_2[DomainA, DomainB, Range]
+{
+    def FROM(relationA: LazyView[DomainA], relationB:  LazyView[DomainB]) =
+        FromWithProjection2 ((tuple: (DomainA, DomainB)) => projection (tuple._1, tuple._2), relationA, relationB, distinct)
+
+}
