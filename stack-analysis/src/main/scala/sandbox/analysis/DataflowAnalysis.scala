fa92d3ccbd89fc0f2f2e2db2e0013edc9443c12e
Mirko <dr.very.best@googlemail.com>
2012-10-26 14:14:31 +0200

    Commit von Mirko

---
+++ stack-analysis/src/main/scala/sandbox/analysis/DataflowAnalysis.scala
@@ -0,0 +1,12 @@
+package sandbox.analysis
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Mirko
+ * Date: 25.10.12
+ * Time: 15:10
+ * To change this template use File | Settings | File Templates.
+ */
+trait DataflowAnalysis {
+
+}
---
+++ stack-analysis/src/main/scala/sandbox/analysis/VarValue.scala
@@ -0,0 +1,12 @@
+package sandbox.analysis
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Mirko
+ * Date: 25.10.12
+ * Time: 16:45
+ * To change this template use File | Settings | File Templates.
+ */
+class VarValue extends Enumeration {
+  val vInt, vFloat, vObject, vLong, vDouble = Value
+}
