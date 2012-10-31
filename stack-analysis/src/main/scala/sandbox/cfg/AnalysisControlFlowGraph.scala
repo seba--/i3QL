812ee473d9512df31b13880437102fcfbfc13e70
Mirko <dr.very.best@googlemail.com>
2012-10-31 15:38:44 +0100

    CFG

--- stack-analysis/src/main/scala/sandbox/cfg/AnalysisControlFlowGraph.scala
+++ stack-analysis/src/main/scala/sandbox/cfg/AnalysisControlFlowGraph.scala
@@ -3,7 +3,8 @@ package sandbox.cfg
 import sae.bytecode.instructions.InstructionInfo
 import sae.{SetRelation, Relation}
 import sae.bytecode.structure.CodeInfo
-import sae.syntax.sql.SELECT
+import sae.syntax.sql._
+
 
 /**
  * This class implements the control flow graph of a program. WILL BE CHANGED!
@@ -14,12 +15,15 @@ import sae.syntax.sql.SELECT
  * Time: 14:33
  * To change this template use File | Settings | File Templates.
  */
-case class AnalysisControlFlowGraph(ci: SetRelation[CodeInfo]) {
+class AnalysisControlFlowGraph(ci: Relation[CodeInfo]) {
+
+  private val codeInfo : Relation[CodeInfo] = ci
 
   private var predecessor : Relation[List[Int]] = null
 
   def computePredecessors : Relation[List[Int]] = {
-    null
+    val res : Relation[String] = compile(SELECT ((c : CodeInfo) => (c.declaringMethod.name)) FROM codeInfo WHERE ((_ : CodeInfo).code == null))
+
   }
 
 
