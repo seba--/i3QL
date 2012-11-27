b6822db32a05ad813c97c2cf9b203f897f1661f4
Ralf Mitschke <mitschke@st.informatik.tu-darmstadt.de>
2012-11-27 18:53:17 +0100

    added oo versions

--- demo/findbugs/src/main/scala/sae/analyses/findbugs/base/oo/Definitions.scala
+++ demo/findbugs/src/main/scala/sae/analyses/findbugs/base/oo/Definitions.scala
@@ -18,72 +18,78 @@ case class Definitions(database: BytecodeDatabase)
     import database._
 
 
-    val serializable = ObjectType ("java/io/Serializable")
+    val serializable = ObjectType("java/io/Serializable")
 
     val subTypesOfSerializable: Relation[ObjectType] =
-        SELECT ((_: InheritanceRelation).subType) FROM (subTypes) WHERE (_.superType == serializable)
+        SELECT((_: InheritanceRelation).subType) FROM (subTypes) WHERE (_.superType == serializable)
 
 
-    val IllegalMonitorStateExceptionType = ObjectType ("java/lang/IllegalMonitorStateException")
+    val IllegalMonitorStateExceptionType = ObjectType("java/lang/IllegalMonitorStateException")
 
-    val cloneable = ObjectType ("java/lang/Cloneable")
+    val cloneable = ObjectType("java/lang/Cloneable")
 
     val subTypesOfCloneable: Relation[ObjectType] =
-        SELECT ((_: InheritanceRelation).subType) FROM (subTypes) WHERE (_.superType == cloneable)
+        SELECT((_: InheritanceRelation).subType) FROM (subTypes) WHERE (_.superType == cloneable)
 
     val implementersOfClone: Relation[MethodDeclaration] =
-        SELECT (*) FROM methodDeclarations WHERE
-            (_.name == "clone") AND
-            (_.parameterTypes == Nil) AND
-            (_.returnType == ObjectType.Object)
+        SELECT(*) FROM methodDeclarations WHERE
+                (_.name == "clone") AND
+                (_.parameterTypes == Nil) AND
+                (_.returnType == ObjectType.Object)
 
-    val comparable = ObjectType ("java/lang/Comparable")
+    val comparable = ObjectType("java/lang/Comparable")
 
     val subTypesOfComparable: Relation[ObjectType] =
-        SELECT ((_: InheritanceRelation).subType) FROM (subTypes) WHERE (_.superType == comparable)
+        SELECT((_: InheritanceRelation).subType) FROM (subTypes) WHERE (_.superType == comparable)
 
     val implementersOfCompareToWithoutObjectParameter: Relation[MethodDeclaration] =
-        SELECT (*) FROM methodDeclarations WHERE
-            (_.name == "compareTo") AND
-            NOT ((_: MethodDeclaration).parameterTypes == Seq (ObjectType.Object)) AND
-            (_.returnType == IntegerType)
+        SELECT(*) FROM methodDeclarations WHERE
+                (_.name == "compareTo") AND
+                NOT((_: MethodDeclaration).parameterTypes == Seq(ObjectType.Object)) AND
+                (_.returnType == IntegerType)
 
-    val system = ObjectType ("java/lang/System")
+    val system = ObjectType("java/lang/System")
 
-    val runtime = ObjectType ("java/lang/Runtime")
+    val runtime = ObjectType("java/lang/Runtime")
 
     val privateFields: Relation[FieldDeclaration] =
-        SELECT (*) FROM fieldDeclarations WHERE (_.isPrivate)
+        SELECT(*) FROM fieldDeclarations WHERE (_.isPrivate)
 
-    val hashTableType = ObjectType ("java/util/Hashtable")
+    val hashTableType = ObjectType("java/util/Hashtable")
 
     def isHashTable: FieldDeclaration => Boolean = field => field.fieldType == hashTableType
 
     def isArray: FieldDeclaration => Boolean = field => field.fieldType.isArrayType
 
-        lazy val fieldReadsFromExternalPackage: Relation[FieldReadInstruction] =
-            SELECT (*) FROM readField WHERE (instruction =>
-                instruction.declaringMethod.declaringClassType.packageName !=
+    lazy val fieldReadsFromExternalPackage: Relation[FieldReadInstruction] =
+        SELECT(*) FROM readField WHERE (instruction =>
+            instruction.declaringMethod.declaringClassType.packageName !=
                     instruction.receiverType.packageName)
 
-    lazy val ms_base : Relation[FieldDeclaration] =
-        SELECT (*) FROM (fieldDeclarations) WHERE
-            (!_.declaringClass.isInterface) AND
-            (_.isStatic) AND
-            (!_.isSynthetic) AND
-            (!_.isVolatile) AND
-            //NOT ((_: FieldDeclaration).isSynthetic) AND
-            //NOT ((_: FieldDeclaration).isVolatile) AND
-            //(((_: FieldDeclaration).isProtected) OR (_.isPublic)) AND
-            (f => f.isProtected || f.isPublic) AND
-            //(isArray OR isHashTable) AND
-            (f => isArray (f) || isHashTable (f)) AND
-            NOT (
-                EXISTS (
-                    SELECT (*) FROM fieldReadsFromExternalPackage WHERE
+    lazy val notInterfaces = compile(
+        SELECT(*) FROM classDeclarations WHERE (!_.isInterface)
+    )
+
+    lazy val ms_fields: Relation[FieldDeclaration] =
+        SELECT((f: FieldDeclaration, c: ClassDeclaration) => f) FROM
+                (fieldDeclarations, notInterfaces) WHERE
+                (_.isStatic) AND
+                (!_.isSynthetic) AND
+                (!_.isVolatile) AND
+                //NOT ((_: FieldDeclaration).isSynthetic) AND
+                //NOT ((_: FieldDeclaration).isVolatile) AND
+                //(((_: FieldDeclaration).isProtected) OR (_.isPublic)) AND
+                (f => f.isProtected || f.isPublic) AND
+                //(isArray OR isHashTable) AND
+                (declaringType === classType)
+
+    lazy val ms_base: Relation[FieldDeclaration] =
+        SELECT(*) FROM ms_fields WHERE NOT(
+            EXISTS(
+                SELECT(*) FROM fieldReadsFromExternalPackage WHERE
                         (((_: FieldReadInstruction).receiverType) === ((_: FieldDeclaration).declaringType)) AND
                         (((_: FieldReadInstruction).name) === ((_: FieldDeclaration).name)) AND
                         (((_: FieldReadInstruction).fieldType) === ((_: FieldDeclaration).fieldType))
-                )
             )
+        )
 }
--- demo/findbugs/src/main/scala/sae/analyses/findbugs/base/relational/Definitions.scala
+++ demo/findbugs/src/main/scala/sae/analyses/findbugs/base/relational/Definitions.scala
@@ -20,79 +20,79 @@ case class Definitions(database: BytecodeDatabase)
     import database._
 
 
-    val serializable = ObjectType ("java/io/Serializable")
+    val serializable = ObjectType("java/io/Serializable")
 
     val subTypesOfSerializable: Relation[ObjectType] =
-        SELECT ((_: InheritanceRelation).subType) FROM (subTypes) WHERE (_.superType == serializable)
+        SELECT((_: InheritanceRelation).subType) FROM (subTypes) WHERE (_.superType == serializable)
 
 
-    val IllegalMonitorStateExceptionType = ObjectType ("java/lang/IllegalMonitorStateException")
+    val IllegalMonitorStateExceptionType = ObjectType("java/lang/IllegalMonitorStateException")
 
-    val cloneable = ObjectType ("java/lang/Cloneable")
+    val cloneable = ObjectType("java/lang/Cloneable")
 
     val subTypesOfCloneable: Relation[ObjectType] =
-        SELECT ((_: InheritanceRelation).subType) FROM (subTypes) WHERE (_.superType == cloneable)
+        SELECT((_: InheritanceRelation).subType) FROM (subTypes) WHERE (_.superType == cloneable)
 
     val implementersOfClone: Relation[MethodDeclaration] =
-        SELECT (*) FROM methodDeclarationsMinimal WHERE
-            (_.name == "clone") AND
-            (_.parameterTypes == Nil) AND
-            (_.returnType == ObjectType.Object)
+        SELECT(*) FROM methodDeclarationsMinimal WHERE
+                (_.name == "clone") AND
+                (_.parameterTypes == Nil) AND
+                (_.returnType == ObjectType.Object)
 
-    val comparable = ObjectType ("java/lang/Comparable")
+    val comparable = ObjectType("java/lang/Comparable")
 
     val subTypesOfComparable: Relation[ObjectType] =
-        SELECT ((_: InheritanceRelation).subType) FROM (subTypes) WHERE (_.superType == comparable)
+        SELECT((_: InheritanceRelation).subType) FROM (subTypes) WHERE (_.superType == comparable)
 
     val implementersOfCompareToWithoutObjectParameter: Relation[MethodDeclaration] =
-        SELECT (*) FROM methodDeclarationsMinimal WHERE
-            (_.name == "compareTo") AND
-            NOT ((_: MethodDeclaration).parameterTypes == Seq (ObjectType.Object)) AND
-            (_.returnType == IntegerType)
+        SELECT(*) FROM methodDeclarationsMinimal WHERE
+                (_.name == "compareTo") AND
+                NOT((_: MethodDeclaration).parameterTypes == Seq(ObjectType.Object)) AND
+                (_.returnType == IntegerType)
 
-    val system = ObjectType ("java/lang/System")
+    val system = ObjectType("java/lang/System")
 
-    val runtime = ObjectType ("java/lang/Runtime")
+    val runtime = ObjectType("java/lang/Runtime")
 
     val privateFields: Relation[FieldDeclaration] =
-        SELECT (*) FROM fieldDeclarationsMinimal WHERE (_.isPrivate)
+        SELECT(*) FROM fieldDeclarationsMinimal WHERE (_.isPrivate)
 
 
     lazy val fieldReadsFromExternalPackage: Relation[FieldReadInstruction] =
-        SELECT (*) FROM readFieldMinimal WHERE (instruction =>
+        SELECT(*) FROM readFieldMinimal WHERE (instruction =>
             instruction.declaringMethod.declaringType.packageName !=
-                instruction.receiverType.packageName)
+                    instruction.receiverType.packageName)
 
-    val hashTableType = ObjectType ("java/util/Hashtable")
+    val hashTableType = ObjectType("java/util/Hashtable")
 
     def isHashTable: FieldDeclaration => Boolean = field => field.fieldType == hashTableType
 
     def isArray: FieldDeclaration => Boolean = field => field.fieldType.isArrayType
 
-    lazy val notInterfaces                         = compile (
-        SELECT (*) FROM classDeclarationsMinimal WHERE (!_.isInterface)
+    lazy val notInterfaces = compile(
+        SELECT(*) FROM classDeclarationsMinimal WHERE (!_.isInterface)
     )
     lazy val ms_fields: Relation[FieldDeclaration] =
-        SELECT ((f: FieldDeclaration, c: ClassDeclaration) => f) FROM
-            (fieldDeclarationsMinimal, notInterfaces) WHERE
-            (_.isStatic) AND
-            (!_.isSynthetic) AND
-            (!_.isVolatile) AND
-            //NOT ((_: FieldDeclaration).isSynthetic) AND
-            //NOT ((_: FieldDeclaration).isVolatile) AND
-            //(((_: FieldDeclaration).isProtected) OR (_.isPublic)) AND
-            (f => f.isProtected || f.isPublic) AND
-            //(isArray OR isHashTable) AND
-            (declaringType === classType)
+        SELECT((f: FieldDeclaration, c: ClassDeclaration) => f) FROM
+                (fieldDeclarationsMinimal, notInterfaces) WHERE
+                (_.isStatic) AND
+                (!_.isSynthetic) AND
+                (!_.isVolatile) AND
+                //NOT ((_: FieldDeclaration).isSynthetic) AND
+                //NOT ((_: FieldDeclaration).isVolatile) AND
+                //(((_: FieldDeclaration).isProtected) OR (_.isPublic)) AND
+                (f => f.isProtected || f.isPublic) AND
+                //(isArray OR isHashTable) AND
+                (declaringType === classType)
 
 
     lazy val ms_base: Relation[FieldDeclaration] =
-        SELECT (*) FROM ms_fields WHERE NOT (
-            EXISTS (
-                SELECT (*) FROM fieldReadsFromExternalPackage WHERE
-                    (((_: FieldReadInstruction).receiverType) === ((_: FieldDeclaration).declaringType)) AND
-                    (((_: FieldReadInstruction).name) === ((_: FieldDeclaration).name)) AND
-                    (((_: FieldReadInstruction).fieldType) === ((_: FieldDeclaration).fieldType))
+        SELECT(*) FROM ms_fields WHERE NOT(
+            EXISTS(
+                SELECT(*) FROM fieldReadsFromExternalPackage WHERE
+                        (((_: FieldReadInstruction).receiverType) === ((_: FieldDeclaration).declaringType)) AND
+                        (((_: FieldReadInstruction).name) === ((_: FieldDeclaration).name)) AND
+                        (((_: FieldReadInstruction).fieldType) === ((_: FieldDeclaration).fieldType))
             )
         )
 
