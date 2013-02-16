2ba614611467815151193cdf64e61a145fbbe6bf
Ralf Mitschke <mitschke@st.informatik.tu-darmstadt.de>
2013-02-16 18:17:40 +0100

    tests

--- demo/analyses/src/main/scala/sae/analyses/simple/CHA.scala
+++ demo/analyses/src/main/scala/sae/analyses/simple/CHA.scala
@@ -2,10 +2,9 @@ package sae.analyses.simple
 
 import sae.bytecode._
 import sae.Relation
-import structure.{MethodDeclaration, InheritanceRelation, MethodInfo}
+import structure.{CodeInfo, MethodDeclaration, InheritanceRelation, MethodInfo}
 import sae.syntax.sql._
 import sae.bytecode.instructions.{InvokeInstruction, InstructionInfo, INVOKEVIRTUAL, INVOKEINTERFACE}
-import sae.operators.impl.ThreeWayJoinView
 
 /**
  * Compute the call graph via Class Hierarchy Analysis (CHA).
@@ -18,7 +17,10 @@ import sae.operators.impl.ThreeWayJoinView
  * @author Ralf Mitschke
  */
 object CHA
-    extends (BytecodeDatabase => Relation[(MethodInfo, MethodInfo)])
+    extends (BytecodeDatabase => Relation[InvokeInstruction])
+    //extends (BytecodeDatabase => Relation[(InvokeInstruction, InheritanceRelation)])
+    //extends (BytecodeDatabase => Relation[(InheritanceRelation, MethodDeclaration)])
+    //extends (BytecodeDatabase => Relation[(MethodDeclaration, MethodDeclaration)])
 {
 
     val invokeInstruction: InstructionInfo => InvokeInstruction = _.asInstanceOf[InvokeInstruction]
@@ -29,14 +31,20 @@ object CHA
 
     val isDynamicInvokeInstruction: InstructionInfo => Boolean = i => i.isInstanceOf[INVOKEVIRTUAL] || i.isInstanceOf[INVOKEINTERFACE]
 
-    val asCallEdge : InvokeInstruction => (MethodInfo, MethodInfo) = i => (i.declaringMethod, i)
+    val asCallEdge: InvokeInstruction => (MethodInfo, MethodInfo) = i => (i.declaringMethod, i)
 
-    def apply(database: BytecodeDatabase): Relation[(MethodInfo, MethodInfo)] = {
+    val enclosingMethod: CodeInfo => (MethodDeclaration) = _.declaringMethod
+
+
+    //def apply(database: BytecodeDatabase): Relation[(InheritanceRelation, MethodDeclaration)] = {
+    //def apply(database: BytecodeDatabase): Relation[(MethodDeclaration, MethodDeclaration)] = {
+    //def apply(database: BytecodeDatabase): Relation[(InvokeInstruction, InheritanceRelation)] = {
+    def apply(database: BytecodeDatabase): Relation[InvokeInstruction] = {
         import database._
 
-        val invokes = compile (
+        val invokes: Relation[InvokeInstruction] = compile (
             SELECT (invokeInstruction) FROM instructions WHERE (isInvokeInstruction)
-        )
+        ).forceToSet
 
         val invokeDynamics = compile (
             SELECT (*) FROM invokes WHERE (isDynamicInvokeInstruction)
@@ -50,19 +58,20 @@ object CHA
         )
         */
 
-
-        val dynamicCalls =
-            new ThreeWayJoinView (
-                invokeDynamics, // left
-                subTypes, // middle
-                methodDeclarations, // right
-                receiverType, // left key
-                superType, // middle to left key
-                subType, // middle to right key
-                declaringType, // right key
-                (invoke: InvokeInstruction, inheritance: InheritanceRelation, declaredMethod: MethodDeclaration) =>
-                    (invoke.declaringMethod, declaringMethod)
-            ).asInstanceOf[Relation[(MethodInfo, MethodInfo)]]
+        /*
+                val dynamicCalls =
+                    new ThreeWayJoinView (
+                        invokeDynamics, // left
+                        subTypes, // middle
+                        methodDeclarations, // right
+                        receiverType, // left key
+                        superType, // middle to left key
+                        subType, // middle to right key
+                        declaringType, // right key
+                        (invoke: InvokeInstruction, inheritance: InheritanceRelation, declaredMethod: MethodDeclaration) =>
+                            (invoke.declaringMethod, declaringMethod)
+                    ).asInstanceOf[Relation[(MethodInfo, MethodInfo)]]
+        */
 
         /*
         compile (
@@ -72,12 +81,42 @@ object CHA
         )
         */
 
+        /*
+                val implementedMethods = compile (
+                    SELECT (enclosingMethod) FROM code
+                )
 
+        */
+
+        val subTypeMethods = compile (
+            SELECT (*) FROM (subTypes, methodDeclarations) WHERE
+                (subType === declaringType)
+        )
+
+        /*
+        val dynamicCalls = compile (
+            SELECT ( (i:InvokeInstruction, x:InheritanceRelation) => i ) FROM (invokeDynamics, subTypes) WHERE
+                (receiverType === superType)
+        )
+        */
+
+
+        /*
         compile (
             SELECT (asCallEdge) FROM invokes UNION_ALL (
                 SELECT (*) FROM dynamicCalls
                 )
         )
+        */
+
+        // //   (i.declaringMethod, x._2)
+
+        val dynamicCalls = compile (
+            SELECT ((i: InvokeInstruction, x: (InheritanceRelation, MethodDeclaration)) => i) FROM (invokeDynamics, subTypeMethods) WHERE
+                (receiverType === ((_: (InheritanceRelation, MethodDeclaration))._1.superType))
+        )
+
+        dynamicCalls
     }
 
 }
