ce22ecb87010aaf668abdf63051b6005db27fa88
Ralf Mitschke <mitschke@st.informatik.tu-darmstadt.de>
2013-02-16 18:59:25 +0100

    cha

--- demo/analyses/src/main/scala/sae/analyses/simple/CHA.scala
+++ demo/analyses/src/main/scala/sae/analyses/simple/CHA.scala
@@ -17,10 +17,8 @@ import sae.bytecode.instructions.{InvokeInstruction, InstructionInfo, INVOKEVIRT
  * @author Ralf Mitschke
  */
 object CHA
-    extends (BytecodeDatabase => Relation[InvokeInstruction])
-    //extends (BytecodeDatabase => Relation[(InvokeInstruction, InheritanceRelation)])
-    //extends (BytecodeDatabase => Relation[(InheritanceRelation, MethodDeclaration)])
-    //extends (BytecodeDatabase => Relation[(MethodDeclaration, MethodDeclaration)])
+    extends (BytecodeDatabase => Relation[(MethodDeclaration, MethodInfo)])
+    //extends (BytecodeDatabase => Relation[InvokeInstruction])
 {
 
     val invokeInstruction: InstructionInfo => InvokeInstruction = _.asInstanceOf[InvokeInstruction]
@@ -31,15 +29,12 @@ object CHA
 
     val isDynamicInvokeInstruction: InstructionInfo => Boolean = i => i.isInstanceOf[INVOKEVIRTUAL] || i.isInstanceOf[INVOKEINTERFACE]
 
-    val asCallEdge: InvokeInstruction => (MethodInfo, MethodInfo) = i => (i.declaringMethod, i)
+    val asCallEdge: InvokeInstruction => (MethodDeclaration, MethodInfo) = i => (i.declaringMethod, i)
 
     val enclosingMethod: CodeInfo => (MethodDeclaration) = _.declaringMethod
 
-
-    //def apply(database: BytecodeDatabase): Relation[(InheritanceRelation, MethodDeclaration)] = {
-    //def apply(database: BytecodeDatabase): Relation[(MethodDeclaration, MethodDeclaration)] = {
-    //def apply(database: BytecodeDatabase): Relation[(InvokeInstruction, InheritanceRelation)] = {
-    def apply(database: BytecodeDatabase): Relation[InvokeInstruction] = {
+    def apply(database: BytecodeDatabase): Relation[(MethodDeclaration, MethodInfo)] = {
+    //def apply(database: BytecodeDatabase): Relation[InvokeInstruction] = {
         import database._
 
         val invokes: Relation[InvokeInstruction] = compile (
@@ -50,73 +45,25 @@ object CHA
             SELECT (*) FROM invokes WHERE (isDynamicInvokeInstruction)
         )
 
-        /*
-        val dynamicCalls = compile (
-            SELECT (*) FROM (invokeDynamics, subTypes, methodDeclarations) WHERE
-                (receiverType == superType) AND
-                (subType == declaringType)
-        )
-        */
-
-        /*
-                val dynamicCalls =
-                    new ThreeWayJoinView (
-                        invokeDynamics, // left
-                        subTypes, // middle
-                        methodDeclarations, // right
-                        receiverType, // left key
-                        superType, // middle to left key
-                        subType, // middle to right key
-                        declaringType, // right key
-                        (invoke: InvokeInstruction, inheritance: InheritanceRelation, declaredMethod: MethodDeclaration) =>
-                            (invoke.declaringMethod, declaringMethod)
-                    ).asInstanceOf[Relation[(MethodInfo, MethodInfo)]]
-        */
-
-        /*
-        compile (
-            SELECT (declaringMethod, invokeTarget) FROM invokes UNION_ALL (
-                SELECT (*) FROM dynamicCalls
-                )
-        )
-        */
-
-        /*
-                val implementedMethods = compile (
-                    SELECT (enclosingMethod) FROM code
-                )
-
-        */
-
         val subTypeMethods = compile (
             SELECT (*) FROM (subTypes, methodDeclarations) WHERE
                 (subType === declaringType)
         )
 
-        /*
         val dynamicCalls = compile (
-            SELECT ( (i:InvokeInstruction, x:InheritanceRelation) => i ) FROM (invokeDynamics, subTypes) WHERE
-                (receiverType === superType)
-        )
-        */
+            SELECT ((i: InvokeInstruction, x: (InheritanceRelation, MethodDeclaration)) => (i.declaringMethod, x._2)) FROM (invokeDynamics, subTypeMethods) WHERE
+                (receiverType === ((_: (InheritanceRelation, MethodDeclaration))._1.superType)) AND
+                (((_:InvokeInstruction).name) === ((_: (InheritanceRelation, MethodDeclaration))._2.name)) AND
+                (((_:InvokeInstruction).returnType) === ((_: (InheritanceRelation, MethodDeclaration))._2.returnType)) AND
+                (((_:InvokeInstruction).parameterTypes) === ((_: (InheritanceRelation, MethodDeclaration))._2.parameterTypes))
+        ).asInstanceOf[Relation[(MethodDeclaration, MethodInfo)]]
 
-
-        /*
         compile (
             SELECT (asCallEdge) FROM invokes UNION_ALL (
                 SELECT (*) FROM dynamicCalls
                 )
         )
-        */
 
-        // //   (i.declaringMethod, x._2)
-
-        val dynamicCalls = compile (
-            SELECT ((i: InvokeInstruction, x: (InheritanceRelation, MethodDeclaration)) => i) FROM (invokeDynamics, subTypeMethods) WHERE
-                (receiverType === ((_: (InheritanceRelation, MethodDeclaration))._1.superType))
-        )
-
-        dynamicCalls
     }
 
 }
