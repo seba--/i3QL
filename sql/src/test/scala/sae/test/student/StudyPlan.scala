9c02c4b220a76d2795da51ba854ed1ae6d85de0b
Ralf Mitschke <mitschke@st.informatik.tu-darmstadt.de>
2013-02-28 09:43:35 +0100

    tests

---
+++ sql/src/test/scala/sae/test/student/StudyPlan.scala
@@ -0,0 +1,31 @@
+package sae.test.student
+
+import sae.{Relation, SetExtent, BagExtent}
+import sae.syntax.sql._
+
+//
+case class CourseDescription(title: String)
+
+
+case class RecommendedBook(course: CourseDescription, author: String, title: String)
+
+case class Lecturer(course: CourseDescription, name: String)
+
+class Database()
+{
+
+    val descriptions: Relation[CourseDescription] =
+        new BagExtent[CourseDescription]
+    val lecturers: Relation[Lecturer] =
+        new SetExtent[Lecturer]
+    val books: Relation[RecommendedBook] =
+        new SetExtent[RecommendedBook]
+
+    def authorName: RecommendedBook => String = _.author
+
+    def lecturerName: Lecturer => String = _.name
+
+    val lecturersWithOwnBook: Relation[Lecturer] =
+        SELECT (first) FROM(lecturers, books) WHERE
+                (lecturerName === authorName)
+}
