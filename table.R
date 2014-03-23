bd9e839f964829387116a96314706fe5675965bc
Sebastian Erdweg <seba.github@gmail.com>
2014-03-23 19:06:43 +0100

    table format

--- table.R
+++ table.R
@@ -2,6 +2,7 @@ source("preparedata.R")
 
 formatd <- function(x) format(round(x, 0), nsmall=0)
 formatp <- function(x) format(round(x, 2), nsmall=2)
+formatt <- function(x) format(round(x, 3), nsmall=3)
 
 outfile <- file.path("evaluation", "table.tex")
 
@@ -9,27 +10,27 @@ firstResult <- 0
 
 
 
-write("\\begin{table*} \\begin{center}",file=outfile)
-write("\\begin{tabular}{ l  r  r | r r r | r r r }",file=outfile,append=TRUE)
-write("& \\multicolumn{2}{ c }{NO-OPT} & \\multicolumn{3}{ c }{PE-OPT}  & \\multicolumn{3}{ c }{ALG-PE-OPT}\\\\",file=outfile,append=TRUE)
+write("\\begin{table}  \\begin{center}",file=outfile)
+write("\\begin{tabular}{ l r| r r | r r }",file=outfile,append=TRUE)
+write("& \\multicolumn{1}{ c }{\\bf NO-OPT} & \\multicolumn{2}{ c }{\\bf PE-OPT}  & \\multicolumn{2}{ c }{\\bf ALG-PE-OPT}\\\\",file=outfile,append=TRUE)
 write("\\toprule",file=outfile,append=TRUE)
-write("\\multicolumn{1}{r|}{} & Time & Memory & Time & Speedup & Memory & Time & Speedup & Memory \\\\",file=outfile,append=TRUE)
+write("\\multicolumn{1}{r|}{} & Time & Time & Speedup & Time & Speedup \\\\",file=outfile,append=TRUE)
 write("\\midrule",file=outfile,append=TRUE)
 i <- 0
 for (s in names) {
   i <- i + 1
   
   optType <- "default"
-  dataTime <- preparecsv5("time", optType, s, skipLines = 0)
-  dataMemory <-  preparecsv5("memory", optType, s, skipLines = 1)
+  dataTime <- preparecsv5("time", optType, s, skipLines=0)
+#  dataMemory <-  preparecsv5("memory", optType, s, skipLines=1)
   
   optType <- "no-opts"
-  dataTimeNoOpts <- preparecsv3("time", optType, s, skipLines = 0)
-  dataMemNoOpts <- preparecsv3("memory", optType, s, skipLines = 1)
+  dataTimeNoOpts <- preparecsv3("time", optType, s, skipLines=0)
+#  dataMemNoOpts <- preparecsv3("memory", optType, s, skipLines=1)
   
   optType <- "no-lms-opts"
-  dataTimeNoLMSOpts <- preparecsv3("time", optType, s, skipLines = 0)
-  dataMemNoLMSOpts <- preparecsv3("memory", optType, s, skipLines = 1)
+  dataTimeNoLMSOpts <- preparecsv3("time", optType, s, skipLines=0)
+#  dataMemNoLMSOpts <- preparecsv3("memory", optType, s, skipLines=1)
   
   dataTimeSum <- sum(dataTime$average)
   dataTimeNoOptsSum <- sum(dataTimeNoOpts$average)
@@ -37,16 +38,16 @@ for (s in names) {
   
   write(paste("\\multicolumn{1}{r|}{", rawToChar(as.raw(64 + i)), "}",
               #NO-OPTS             
-              " & ", formatd(dataTimeNoLMSOptsSum) ,   
-              " & ", formatd(sum(dataMemNoLMSOpts$average)) ,                         
+              " & ", formatt(dataTimeNoLMSOptsSum / 1000) ,   
+#              " & ", formatd(sum(dataMemNoLMSOpts$average)) ,                         
               #LMS_OPTS
-              " & ", formatd(dataTimeNoOptsSum) ,
+              " & ", formatt(dataTimeNoOptsSum / 1000) ,
               " & ", formatp(dataTimeNoLMSOptsSum / dataTimeNoOptsSum),
-              " & ", formatd(sum(dataMemNoOpts$average)) ,
+#              " & ", formatd(sum(dataMemNoOpts$average)) ,
               #ALG-LMS-OPTS
-              " & ", formatd(dataTimeSum),
+              " & ", formatt(dataTimeSum / 1000),
               " & ", formatp(dataTimeNoLMSOptsSum / dataTimeSum),
-              " & ", formatd(sum(dataMemory$average)),              
+#              " & ", formatd(sum(dataMemory$average)),              
               
               " \\\\" ,sep=""),file=outfile,append=TRUE)
   
@@ -57,10 +58,10 @@ for (s in names) {
 write("\\bottomrule",file=outfile,append=TRUE)
 write("\\end{tabular}",file=outfile,append=TRUE)
 write("\\end{center}",file=outfile,append=TRUE)
-write("{\\small Bug patterns of the analyses similar to FindBugs: A = BX\\_BOXING\\_IMMEDIATELY\\_UNBOXED\\_TO\\_PERFORM\\_COERCION, B = CI\\_CONFUSED\\_INHERITANCE, C = CN\\_IDIOM, D = CN\\_IDIOM\\_NO\\_SUPER\\_CALL, E = DM\\_GC, F = DM\\_RUN\\_FINALIZERS\\_ON\\_EXIT, G = DP\\_DO\\_INSIDE\\_DO\\_PRIVILEGED, H = EQ\\_ABSTRACT\\_SELF, I = FI\\_PUBLIC\\_SHOULD\\_BE\\_PROTECTED, J = IMSE\\_DONT\\_CATCH\\_IMSE, K = MS\\_PKGPROTECT, L = MS\\_SHOULD\\_BE\\_FINAL, M = SE\\_BAD\\_FIELD\\_INNER\\_CLASS, N = SW\\_SWING\\_METHODS\\_INVOKED\\_IN\\_SWING\\_THREAD, O = UG\\_SYNC\\_SET\\_UNSYNC\\_GET}",file=outfile,append=TRUE)
-write("\\caption{Total duration (in ms) and total memory usage (in KiB) for the static program analyses, as well as the time speed up relative to NO-OPT.}",file=outfile,append=TRUE)
+write("{\\tiny FindBugs patterns from \\href{http://findbugs.sourceforge.net/bugDescriptions.html}{http://findbugs.sourceforge.net/bugDescriptions.html}: A=BX\\textunderscore\\allowbreak{}BOXING\\textunderscore\\allowbreak{}IMMEDIATELY\\textunderscore\\allowbreak{}UNBOXED\\textunderscore\\allowbreak{}TO\\textunderscore\\allowbreak{}PERFORM\\textunderscore\\allowbreak{}COERCION, B=CI\\textunderscore\\allowbreak{}CONFUSED\\textunderscore\\allowbreak{}INHERITANCE, C=CN\\textunderscore\\allowbreak{}IDIOM, D=CN\\textunderscore\\allowbreak{}IDIOM\\textunderscore\\allowbreak{}NO\\textunderscore\\allowbreak{}SUPER\\textunderscore\\allowbreak{}CALL, E=DM\\textunderscore\\allowbreak{}GC, F=DM\\textunderscore\\allowbreak{}RUN\\textunderscore\\allowbreak{}FINALIZERS\\textunderscore\\allowbreak{}ON\\textunderscore\\allowbreak{}EXIT, G=DP\\textunderscore\\allowbreak{}DO\\textunderscore\\allowbreak{}INSIDE\\textunderscore\\allowbreak{}DO\\textunderscore\\allowbreak{}PRIVILEGED, H=EQ\\textunderscore\\allowbreak{}ABSTRACT\\textunderscore\\allowbreak{}SELF, I=FI\\textunderscore\\allowbreak{}PUBLIC\\textunderscore\\allowbreak{}SHOULD\\textunderscore\\allowbreak{}BE\\textunderscore\\allowbreak{}PROTECTED, J=IMSE\\textunderscore\\allowbreak{}DONT\\textunderscore\\allowbreak{}CATCH\\textunderscore\\allowbreak{}IMSE, K=MS\\textunderscore\\allowbreak{}PKGPROTECT, L=MS\\textunderscore\\allowbreak{}SHOULD\\textunderscore\\allowbreak{}BE\\textunderscore\\allowbreak{}FINAL, M=SE\\textunderscore\\allowbreak{}BAD\\textunderscore\\allowbreak{}FIELD\\textunderscore\\allowbreak{}INNER\\textunderscore\\allowbreak{}CLASS, N=SW\\textunderscore\\allowbreak{}SWING\\textunderscore\\allowbreak{}METHODS\\textunderscore\\allowbreak{}INVOKED\\textunderscore\\allowbreak{}IN\\textunderscore\\allowbreak{}SWING\\textunderscore\\allowbreak{}THREAD, O=UG\\textunderscore\\allowbreak{}SYNC\\textunderscore\\allowbreak{}SET\\textunderscore\\allowbreak{}UNSYNC\\textunderscore\\allowbreak{}GET\\par}",file=outfile,append=TRUE)
+write("\\caption{Duration in seconds, speed up relative to NO-OPT.}",file=outfile,append=TRUE)
 write("\\label{tbl:analysisbenchmarks}",file=outfile,append=TRUE)
-write("\\end{table*}",file=outfile,append=TRUE)
+write("\\end{table}",file=outfile,append=TRUE)
 
 firstResult <- firstResult / length(names)
 print(firstResult)
