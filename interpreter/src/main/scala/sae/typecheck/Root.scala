2a5c577635ca51f973436c69f25b79350bd2636d
Sebastian Erdweg <seba.github@gmail.com>
2014-10-28 14:57:53 +0100

    generalize root for different typing relations

--- interpreter/src/main/scala/sae/typecheck/Root.scala
+++ interpreter/src/main/scala/sae/typecheck/Root.scala
@@ -18,28 +18,24 @@ class Root[V](private var rootNode: Exp) {
     rootNode = newroot
   }
 
-  var Type: V = scala.Right("Uninitialized root")
-  def store(t: Either[Type, TError]) = t match {
-    case scala.Left(Root.TRoot(t)) => Type = scala.Left(t)
-    case scala.Right(msg) => Type = scala.Right(msg)
-    case _ => throw new RuntimeException(s"Unexpected root type $t")
-  }
+  var Type: V = _
+  def store(t: V) = Type = t
 }
 
 object Root {
   case object Root extends ExpKind
   case class TRoot(t: Type) extends Type
 
-  def apply[V](types: Relation[V]): Root[V] = {
+  def apply[V : Manifest](types: Relation[(Exp.ExpKey, V)]): Root[V] = {
     val rootNode = Exp(Root, scala.Seq(), scala.Seq())
     val root = new Root[V](rootNode)
     val rootKey: Rep[Int] = Exp.prefetchKey
-    val rootQuery = SELECT ((t: Rep[TypeTuple]) => t._2) FROM types WHERE (t => tid(t) == rootKey)
-    rootQuery.addObserver(new Observer[Either[Type, TError]] {
-      override def updated(oldV: Either[Type, TError], newV: Either[Type, TError]): Unit = root.store(newV)
+    val rootQuery = SELECT ((t: Rep[(Exp.ExpKey, V)]) => t._2) FROM types WHERE (t => t._1 == rootKey)
+    rootQuery.addObserver(new Observer[V] {
+      override def updated(oldV: V, newV: V): Unit = root.store(newV)
+      override def removed(v: V): Unit = root.store(null.asInstanceOf[V])
+      override def added(v: V): Unit = root.store(v)
       override def endTransaction(): Unit = {}
-      override def removed(v: Either[Type, TError]): Unit = root.store(scala.Right("Uninitialized root"))
-      override def added(v: Either[Type, TError]): Unit = root.store(v)
     })
     rootNode.insert
     root
