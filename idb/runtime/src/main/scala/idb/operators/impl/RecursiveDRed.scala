5b215ebd48987df663764db0d10432151f6b76a4
Sebastian Erdweg <seba.github@gmail.com>
2014-11-10 15:18:03 +0100

    support batch commits for recursive operator

--- idb/runtime/src/main/scala/idb/operators/impl/RecursiveDRed.scala
+++ idb/runtime/src/main/scala/idb/operators/impl/RecursiveDRed.scala
@@ -45,239 +45,237 @@ import scala.collection.mutable
  */
 
 class RecursiveDRed[Domain](val relation: Relation[Domain],
-							override val isSet : Boolean,
-							val transactional : Boolean = false
-	)
-    extends Recursive[Domain]
-	with NotifyObservers[Domain]
-{
+                            override val isSet: Boolean,
+                            val transactional: Boolean = false
+                             )
+  extends Recursive[Domain]
+  with NotifyObservers[Domain] {
 
-    relation.addObserver (this)
+  relation.addObserver(this)
 
-    // the set of elements in the current recursion
-    // recording them avoids endless recursions
-    //
-    // the recursive path taken to support the currently added value
-    private var currentSupportPath: List[Domain] = Nil
+  // the set of elements in the current recursion
+  // recording them avoids endless recursions
+  //
+  // the recursive path taken to support the currently added value
+  private var currentSupportPath: List[Seq[Domain]] = Nil
 
-    // these elements were already derived once and will not be propagated a second time
-    private var supportedElements: mutable.HashMap[Domain, Int] = mutable.HashMap.empty
+  // these elements were already derived once and will not be propagated a second time
+  private var supportedElements: mutable.HashMap[Domain, Int] = mutable.HashMap.empty
 
-    private var deletedElements: mutable.HashMap[Domain, Int] = mutable.HashMap.empty
+  private var deletedElements: mutable.HashMap[Domain, Int] = mutable.HashMap.empty
 
-    private def mergeCurrentSupportTo(v: Domain) {
-        val supportingPathCount = supportedElements (v)
-        supportedElements (v) = supportingPathCount + 1
-    }
-
-
-    private def deleteCurrentSupportTo(v: Domain) {
-        val supportingPathCount = deletedElements (v)
-        deletedElements (v) = supportingPathCount + 1
-    }
-
-
-    /**
-     * recursion stacks will look lick this
-     * Nil
-     * List(Nil)
-     * List(List(v), Nil)
-     * List(List(v), List(u), Nil)
-     * List(List(v), List(u,w), Nil)
-     * List(List(v), List(u), Nil)
-     * List(List(v), List(u), Nil)
-     * List(List(v), Nil)
-     * List(Nil)
-     * Nil
-     */
-    private var additionRecursionStack: List[List[Domain]] = Nil
-
-
-    private var deletionRecursionStack: List[List[Domain]] = Nil
-
-    // delete supports
-    private var rederivations: List[Domain] = Nil
-
-	override def lazyInitialize() {
-
-	}
-
-    def added(v: Domain) {
-        if (supportedElements.contains (v)) {
-            // we have reached a value that was previously defined.
-
-            // add the current recursion to the support for v
-            mergeCurrentSupportTo (v)
-            return
-        }
-        else
-        {
-            supportedElements (v) = 1
-        }
-
-        if (!additionRecursionStack.isEmpty) {
-            // we have derived a new value in the recursion
-            // it will be treated in the enclosing call
-
-            // basically everything that ends up here comes from one single
-            // call to element_added, i.e., it is all derived at the same recursion depth
-
-            // add v as a derived value at the current recursion depth
-            additionRecursionStack = (v :: additionRecursionStack.head) :: additionRecursionStack.tail
-            return
-        }
-
-
-        // we have reached the start of a recursion
-
-        additionRecursionStack = List (List (v))
-        while (!additionRecursionStack.isEmpty) {
-            // we have derived next and now we want to derive further values recursively
-            val next = additionRecursionStack.head.head
-            // remove the current value from the current level
-            additionRecursionStack = additionRecursionStack.head.tail :: additionRecursionStack.tail
-            // add a new empty list at the beginning of the level
-            additionRecursionStack = Nil :: additionRecursionStack
-            // next is a support on the current recursive path
-            currentSupportPath = next :: currentSupportPath
-            // add elements of the next level
-            //println("Added:  " + next)
-            notify_added (next)
-
-            // we did not compute a new level, i.e., the next recursion level of values is empty
-            // remove all empty levels
-            while (!additionRecursionStack.isEmpty && additionRecursionStack.head == Nil) {
-                additionRecursionStack = additionRecursionStack.tail
-                if (!currentSupportPath.isEmpty) {
-                    currentSupportPath = currentSupportPath.tail
-                }
-            }
-        }
-
-    }
-
-  // TODO more efficient version possible?
-  def addedAll(vs: Seq[Domain]): Unit = {
-    println(s"WARNING: Running slow incremental DRed recursive add, size ${vs.size}")
-    vs foreach (added(_))
+  private def mergeCurrentSupportTo(v: Domain) {
+    val supportingPathCount = supportedElements(v)
+    supportedElements(v) = supportingPathCount + 1
   }
 
-    def removed(v: Domain) {
-        if (deletedElements.contains (v)) {
-            // we have reached a value that was previously defined.
 
-            // add the current recursion to the support for v
-            deleteCurrentSupportTo (v)
-            return
-        }
-        else
-        {
-            deletedElements (v) = 1
-        }
-
-        if (!deletionRecursionStack.isEmpty) {
-            // we have derived a new value in the recursion
-            // it will be treated in the enclosing call
-
-            // basically everything that ends up here comes from one single
-            // call to element_added, i.e., it is all derived at the same recursion depth
-
-            // add v as a derived value at the current recursion depth
-            deletionRecursionStack = (v :: deletionRecursionStack.head) :: deletionRecursionStack.tail
-            return
-        }
-
-
-        // we have reached the start of a recursion
-
-        deletionRecursionStack = List (List (v))
-        while (!deletionRecursionStack.isEmpty) {
-            // we have derived next and now we want to derive further values recursively
-            val next = deletionRecursionStack.head.head
-            // remove the current value from the current level
-            deletionRecursionStack = deletionRecursionStack.head.tail :: deletionRecursionStack.tail
-            // add a new empty list at the beginning of the level
-            deletionRecursionStack = Nil :: deletionRecursionStack
-            // next is a support on the current recursive path
-            currentSupportPath = next :: currentSupportPath
-            // add elements of the next level
-            //println("Remove: " + next)
-			notify_removed (next)
-
-            // we did not compute a new level, i.e., the next recursion level of values is empty
-            // remove all empty levels
-            while (!deletionRecursionStack.isEmpty && deletionRecursionStack.head == Nil) {
-                deletionRecursionStack = deletionRecursionStack.tail
-                if (!currentSupportPath.isEmpty) {
-                    currentSupportPath = currentSupportPath.tail
-                }
-            }
-        }
-
-
-
-
-        for ((key, deletionCount) <- deletedElements) {
-			val newSupportCount = supportedElements (key) - deletionCount
-			if (newSupportCount > 0) {
-				supportedElements (key) = newSupportCount
-				rederivations = key :: rederivations
-			}
-			else
-			{
-				supportedElements.remove (key)
-			}
-        }
-
-        deletedElements = mutable.HashMap.empty
-
-        // rederive
-        rederivations.foreach ( e =>
-        {
-            //println("Rederive:" + e)
-			notify_added(e)
-        }
-        )
-        rederivations = Nil
-    }
-
-  // TODO more efficient version possible?
-  def removedAll(vs: Seq[Domain]): Unit = {
-    println(s"WARNING: Running slow incremental DRed recursive remove, size ${vs.size}")
-    vs foreach (removed(_))
+  private def deleteCurrentSupportTo(v: Domain) {
+    val supportingPathCount = deletedElements(v)
+    deletedElements(v) = supportingPathCount + 1
   }
 
-    def updated(oldV: Domain, newV: Domain) {
-        if (!additionRecursionStack.isEmpty) {
-            // we are currently adding elements
-            supportedElements(oldV) = supportedElements(oldV) - 1
-            added (newV)
-        }
-        else if (!deletionRecursionStack.isEmpty) {
-            // we are currently deleting elements
-            removed (oldV)
-            supportedElements(newV) = supportedElements.getOrElse(newV, 0) + 1
-            rederivations = newV :: rederivations
-        }
-        else
-        {
-            removed (oldV)
-            added (newV)
-        }
+
+  /**
+   * recursion stacks will look lick this
+   * Nil
+   * List(Nil)
+   * List(List(v), Nil)
+   * List(List(v), List(u), Nil)
+   * List(List(v), List(u,w), Nil)
+   * List(List(v), List(u), Nil)
+   * List(List(v), List(u), Nil)
+   * List(List(v), Nil)
+   * List(Nil)
+   * Nil
+   */
+  private var additionRecursionStack: List[Seq[Domain]] = Nil
+
+
+  private var deletionRecursionStack: List[Seq[Domain]] = Nil
+
+  // delete supports
+  private var rederivations: Seq[Domain] = Seq()
+
+  override def lazyInitialize() {
+
+  }
+
+  def added(v: Domain) {
+    addedAll(Seq(v))
+  }
+
+  def addedAll(vs: Seq[Domain]) {
+    println(s"Recursive add, size ${vs.size}")
+
+    val newVs = vs filter {v =>
+      if (supportedElements.contains(v)) {
+        // we have reached a value that was previously defined.
+
+        // add the current recursion to the support for v
+        mergeCurrentSupportTo(v)
+        false
+      }
+      else {
+        supportedElements(v) = 1
+        true
+      }
     }
 
+    if (newVs.isEmpty)
+      return
 
-    var isNotifyingEndTransaction = false
+    if (!additionRecursionStack.isEmpty) {
+      // we have derived a new value in the recursion
+      // it will be treated in the enclosing call
 
-    override def endTransaction() {
-        if (transactional) {
-            supportedElements = mutable.HashMap.empty
-        }
-        if (!isNotifyingEndTransaction) {
-            isNotifyingEndTransaction = true
-            notify_endTransaction ()
-        }
-        isNotifyingEndTransaction = false
+      // basically everything that ends up here comes from one single
+      // call to element_added, i.e., it is all derived at the same recursion depth
+
+      // add v as a derived value at the current recursion depth
+      additionRecursionStack = (additionRecursionStack.head ++ newVs) :: additionRecursionStack.tail
     }
+    else {
+      // we have reached the start of a recursion
+
+      additionRecursionStack = List(newVs)
+      while (!additionRecursionStack.isEmpty) {
+        // we have derived next and now we want to derive further values recursively
+        val nexts = additionRecursionStack.head
+        // remove the current value from the current level
+        additionRecursionStack = Seq() :: additionRecursionStack.tail
+        // add a new empty list at the beginning of the level
+        additionRecursionStack = Seq() :: additionRecursionStack
+        // next is a support on the current recursive path
+        currentSupportPath = nexts :: currentSupportPath
+        // add elements of the next level
+        //println("Added:  " + next)
+        notify_addedAll(nexts)
+
+        // we did not compute a new level, i.e., the next recursion level of values is empty
+        // remove all empty levels
+        while (!additionRecursionStack.isEmpty && additionRecursionStack.head.isEmpty) {
+          additionRecursionStack = additionRecursionStack.tail
+          if (!currentSupportPath.isEmpty) {
+            currentSupportPath = currentSupportPath.tail
+          }
+        }
+      }
+    }
+  }
+
+  def removed(v: Domain) {
+    removedAll(Seq(v))
+  }
+
+  def removedAll(vs: Seq[Domain]) {
+    println(s"Recursive rem, size ${vs.size}")
+    
+    val newVs = vs filter { v =>
+      if (deletedElements.contains(v)) {
+        // we have reached a value that was previously defined.
+
+        // add the current recursion to the support for v
+        deleteCurrentSupportTo(v)
+        false
+      }
+      else {
+        deletedElements(v) = 1
+        true
+      }
+    }
+
+    if (newVs.isEmpty)
+      return
+
+    if (!deletionRecursionStack.isEmpty) {
+      // we have derived a new value in the recursion
+      // it will be treated in the enclosing call
+
+      // basically everything that ends up here comes from one single
+      // call to element_added, i.e., it is all derived at the same recursion depth
+
+      // add v as a derived value at the current recursion depth
+      deletionRecursionStack = (deletionRecursionStack.head ++ newVs) :: deletionRecursionStack.tail
+      return
+    }
+    else {
+      // we have reached the start of a recursion
+
+      deletionRecursionStack = List(newVs)
+
+      while (!deletionRecursionStack.isEmpty) {
+        // we have derived next and now we want to derive further values recursively
+        val nexts = deletionRecursionStack.head
+        // remove the current value from the current level
+        deletionRecursionStack = Seq() :: deletionRecursionStack.tail
+        // add a new empty list at the beginning of the level
+        deletionRecursionStack = Seq() :: deletionRecursionStack
+        // next is a support on the current recursive path
+        currentSupportPath = nexts :: currentSupportPath
+        // add elements of the next level
+        //println("Remove: " + next)
+        notify_removedAll(nexts)
+
+        // we did not compute a new level, i.e., the next recursion level of values is empty
+        // remove all empty levels
+        while (!deletionRecursionStack.isEmpty && deletionRecursionStack.head.isEmpty) {
+          deletionRecursionStack = deletionRecursionStack.tail
+          if (!currentSupportPath.isEmpty) {
+            currentSupportPath = currentSupportPath.tail
+          }
+        }
+      }
+
+      for ((key, deletionCount) <- deletedElements) {
+        val newSupportCount = supportedElements(key) - deletionCount
+        if (newSupportCount > 0) {
+          supportedElements(key) = newSupportCount
+          rederivations = key +: rederivations
+        }
+        else {
+          supportedElements.remove(key)
+        }
+      }
+
+      deletedElements = mutable.HashMap.empty
+
+      //println("Rederive: " + rederivations)
+      notify_addedAll(rederivations)
+      rederivations = Seq()
+    }
+  }
+
+  def updated(oldV: Domain, newV: Domain) {
+    if (!additionRecursionStack.isEmpty) {
+      // we are currently adding elements
+      supportedElements(oldV) = supportedElements(oldV) - 1
+      added(newV)
+    }
+    else if (!deletionRecursionStack.isEmpty) {
+      // we are currently deleting elements
+      removed(oldV)
+      supportedElements(newV) = supportedElements.getOrElse(newV, 0) + 1
+      rederivations = newV +: rederivations
+    }
+    else {
+      removed(oldV)
+      added(newV)
+    }
+  }
+
+
+  var isNotifyingEndTransaction = false
+
+  override def endTransaction() {
+    if (transactional) {
+      supportedElements = mutable.HashMap.empty
+    }
+    if (!isNotifyingEndTransaction) {
+      isNotifyingEndTransaction = true
+      notify_endTransaction()
+    }
+    isNotifyingEndTransaction = false
+  }
 }
 
