62af66207fc4b973d5eec13862091b19e31b2da0
Sebastian Erdweg <seba.github@gmail.com>
2014-11-12 10:52:31 +0100

    bottomup type checking without i3QL

---
+++ typecheck/src/main/scala/sae/typecheck/bottomup/Nonrelational.scala
@@ -0,0 +1,185 @@
+package sae.typecheck.bottomup
+
+import sae.typecheck.Constraint._
+import sae.typecheck.Exp.ExpKind
+import sae.typecheck.TypeStuff._
+import sae.typecheck._
+
+/**
+ * Created by seba on 12/11/14.
+ */
+object Nonrelational extends TypeCheck {
+
+  def reset() {}
+  def typecheckIncremental(e: Exp): Either[Type, TError] = typecheck(e)()
+
+
+  def typecheck(e: Exp): () => Either[Type, TError] = {
+    val (root, leaves) = BUExp.from[ConstraintSolutionData](e)
+
+    () => {
+      leaves foreach (typecheckSpine(_))
+
+      val (t, sol, reqs, free) = root.Type
+      if (!reqs.isEmpty)
+        scala.Right(s"Unresolved context requirements $reqs, type $t, solution $sol, free $free")
+      else if (sol._2.isEmpty)
+        scala.Left(t)
+      else
+        scala.Right(s"Unresolved constraints ${sol._2}, solution $sol, free $free")
+    }
+  }
+
+  def typecheckSpine(e: BUExp[ConstraintSolutionData]): Unit ={
+    val isFirstTime = e.Type == null
+    val isRoot = e.parent == null
+
+    val t = typecheckStep(e)
+    if (e.Type != t) {
+      e.Type = t
+      if (!isRoot && isFirstTime)
+        e.parent.markKidTypeAvailable(e.pos)
+      if (!isRoot && e.parent.allKidTypesAvailable)
+        typecheckSpine(e.parent)
+    }
+  }
+
+  def typecheckStep(e: BUExp[ConstraintSolutionData]): ConstraintSolutionData = e.kind match {
+    case (Num, 0) => (TNum, (Map(), Set()), Set(), Set())
+    case (op, 2) if op == Add || op == Mul =>
+      val (t1, sol1, reqs1, free1) = e.kids(0).Type
+      val (_t2, _sol2, _reqs2, free2) = e.kids(1).Type
+      val (mfresh, ren) = mergeFresh(free1, free2)
+      val (t2, sol2, reqs2) = renameSolution(ren)(_t2, _sol2, _reqs2)
+
+      val lcons = EqConstraint(TNum, t1)
+      val rcons = EqConstraint(TNum, t2)
+      val (mcons, mreqs) = mergeReqs(reqs1, reqs2)
+
+      val sol12 = mergeSolution(sol1, sol2)
+      val sol = extendSolution(sol12, mcons + lcons + rcons)
+      (TNum, sol, mreqs, mfresh)
+    case (Var, 0) =>
+      val x = e.lits(0).asInstanceOf[Symbol]
+      val X = TVar(Symbol("X$" + x.name))
+      (X, (Map(), Set()), Set(VarRequirement(x, X)), Set(X.x))
+    case (App, 2) =>
+      val (t1, sol1, reqs1, fresh1) = e.kids(0).Type
+      val (_t2, _sol2, _reqs2, fresh2) = e.kids(1).Type
+      val (mfresh, ren) = mergeFresh(fresh1, fresh2)
+      val (t2, sol2, reqs2) = renameSolution(ren)(_t2, _sol2, _reqs2)
+
+      val X = TVar(tick('X$App, mfresh))
+      val fcons = EqConstraint(TFun(t2, X), t1)
+      val (mcons, mreqs) = mergeReqs(reqs1, reqs2)
+
+      val sol12 = mergeSolution(sol1, sol2)
+      val sol = extendSolution(sol12, mcons + fcons)
+
+      (X.subst(sol._1), sol, mreqs, mfresh + X.x)
+    case (Abs, 1) =>
+      val x = e.lits(0).asInstanceOf[Symbol]
+      val (t, sol, reqs, fresh) = e.kids(0).Type
+
+      val X = TVar(tick('X$Abs, fresh))
+      val ((otherReqs, xcons), time) = Util.timed {
+        val (xreqs, otherReqs) = reqs.partition { case VarRequirement(`x`, _) => true; case _ => false}
+        val xcons = xreqs map { case VarRequirement(_, t) => EqConstraint(X, t.subst(sol._1))}
+        (otherReqs, xcons)
+      }
+      Constraint.computeReqsTime += time
+      val fsol = extendSolution(sol, xcons)
+      (TFun(X, t).subst(fsol._1), fsol, otherReqs, fresh + X.x)
+    case (If0, 3) =>
+      val (t1, sol1, reqs1, fresh1) = e.kids(0).Type
+      val (_t2, _sol2, _reqs2, fresh2) = e.kids(1).Type
+      val (_t3, _sol3, _reqs3, fresh3) = e.kids(2).Type
+      val (mfresh12, ren12) = mergeFresh(fresh1, fresh2)
+      val (t2, sol2, reqs2) = renameSolution(ren12)(_t2, _sol2, _reqs2)
+      val (mfresh123, ren23) = mergeFresh(mfresh12, fresh3)
+      val (t3, sol3, reqs3) = renameSolution(ren23)(_t3, _sol3, _reqs3)
+
+      val (mcons12, mreqs12) = mergeReqs(reqs1, reqs2)
+      val (mcons23, mreqs123) = mergeReqs(mreqs12, reqs3)
+
+      val cond = EqConstraint(TNum, t1)
+      val body = EqConstraint(t2, t3)
+
+      val sol123 = mergeSolution(sol1, mergeSolution(sol2, sol3))
+      val sol = extendSolution(sol123, mcons12 ++ mcons23 + cond + body)
+
+      (t2.subst(sol._1), sol, mreqs123, mfresh123)
+
+    case (Fix, 1) =>
+      val (t, sol, reqs, fresh) = e.kids(0).Type
+      val X = TVar(tick('X$Fix, fresh))
+      val fixCons = EqConstraint(t, TFun(X, X))
+      val fsol = extendSolution(sol, Set(fixCons))
+      (X.subst(fsol._1), fsol, reqs, fresh + X.x)
+  }
+
+
+  def main(args: Array[String]): Unit = {
+    printTypecheck(Add(Num(17), Add(Num(10), Num(2))))
+    printTypecheck(Add(Num(17), Add(Num(10), Num(5))))
+    printTypecheck(Abs('x, Add(Num(10), Num(5))))
+    printTypecheck(Abs('x, Add(Var('x), Var('x))))
+    printTypecheck(Abs('x, Add(Var('err), Var('x))))
+    printTypecheck(Abs('x, Abs('y, App(Var('x), Var('y)))))
+    printTypecheck(Abs('x, Abs('y, Add(Var('x), Var('y)))))
+    printTypecheck(If0(Num(17), Num(0), Num(1)))
+
+    val fac = Fix(Abs('f, Abs('n, If0(Var('n), Num(1), Mul(Var('n), App(Var('f), Add(Var('n), Num(-1))))))))
+    printTypecheck("factorial", fac)
+    printTypecheck("eta-expanded factorial", Abs('x, App(fac, Var('x))))
+
+    val fib = Fix(Abs('f, Abs('n,
+      If0(Var('n), Num(1),
+        If0(Add(Var('n), Num(-1)), Num(1),
+          Add(App(Var('f), Add(Var('n), Num(-1))),
+            App(Var('f), Add(Var('n), Num(-2)))))))))
+    printTypecheck("fibonacci function", fib)
+    printTypecheck("factorial+fibonacci", Abs('x, Add(App(fac, Var('x)), App(fib, Var('x)))))
+    printTypecheck(Abs('y, Var('y)))
+  }
+}
+
+
+object BUExp {
+  def from[T](e: Exp): (BUExp[T], Seq[BUExp[T]]) = {
+    val leaves = collection.mutable.ArrayBuffer[BUExp[T]]()
+    val bue = convert[T](e, null, -1, leaves)
+    (bue, leaves.toSeq)
+  }
+
+  private def convert[T](e: Exp, parent: BUExp[T], pos: Int, leaves: collection.mutable.ArrayBuffer[BUExp[T]]): BUExp[T] = {
+    val bue = BUExp[T](e.kind, e.lits,parent, pos)
+    if (e.sub.isEmpty) {
+      bue.kids = Seq()
+      leaves += bue
+    }
+    else {
+      val kids = for (i <- 0 until e.sub.size)
+        yield convert[T](e.sub(i), bue, i, leaves)
+      bue.kids = kids
+    }
+    bue
+  }
+}
+case class BUExp[T](kind: ExpKind, lits: Seq[Any], parent: BUExp[T], pos: Int) {
+  private var _kids: Seq[BUExp[T]] = _
+  var availableKidTypes: Seq[Boolean] = _
+
+  var Type: T = _
+
+  def kids = _kids
+  def kids_=(es: Seq[BUExp[T]]) = {
+    _kids = es
+    availableKidTypes = es map (_.Type != null)
+  }
+
+  def markKidTypeAvailable(pos: Int) =
+    availableKidTypes = availableKidTypes.updated(pos, true)
+
+  def allKidTypesAvailable = availableKidTypes.foldLeft(true)(_&&_)
+}
