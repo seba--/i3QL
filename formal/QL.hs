3fb57d76a1129eb1df31152531b33af02f175f5a
Sebastian Erdweg <seba.github@gmail.com>
2015-06-23 11:20:14 +0200

    comments

--- formal/QL.hs
+++ formal/QL.hs
@@ -5,12 +5,12 @@ module QL where
 import Data.Maybe
 import qualified Data.Map as Map
 
+-- Query Language
 data QL t a b where
      Map :: (a -> b) -> QL t (t a) (t b)
      Filter :: (a -> Bool) -> QL t (t a) (t a)
      Fold :: (a -> b -> b) -> (b -> b -> b) -> b -> QL t (t a) b
      Join :: Ord k => (a -> k) -> (b -> k) -> (a -> b -> c) -> QL t (t a,t b) (t c)
---     IxJoin :: Ord k => (a -> b -> c) -> Map.Map k [a] -> Map.Map k [b] -> QL () c
      Localize :: QL t (t a) (t a)
      Materialize :: QL t (t a) (t a)
 
@@ -21,6 +21,8 @@ class Table t where
   telems :: t a -> [a]
   eval :: QL t a b -> a -> b
 
+
+-- # Tables as lists. Non-parallel. Non-incremental.
 instance Table [] where
   tempty = []
   tmake = id
@@ -49,12 +51,13 @@ merge f ix1 ix2 = Map.mapWithKey (\k x -> crossApply f x (get k ix2))
 crossApply :: (a -> b -> c) -> [a] -> [b] -> [c]
 crossApply f xs ys = [f x y | x <- xs, y <- ys]
 
-mapExtend :: Ord k => k -> b -> (b -> b) -> Map.Map k b -> Map.Map k b
-mapExtend k noth just m = Map.alter (Just. maybe noth just) k m
 
+-- # Tables distributed over multiple machines. 
 
+-- ID identifies different machines.
 type ID = Int
 
+-- Data is partitioned over multiple machines.
 data PData t a where
      Partition :: Table t => Map.Map ID (t a) -> PData t a
 
@@ -66,21 +69,33 @@ instance Table t => Table (PData t) where
   
   eval (Map f) (Partition ts) = Partition $ Map.map (eval (Map f)) ts 
   eval (Filter f) (Partition ts) = Partition $ Map.map (eval (Filter f)) ts
-  eval (Fold f m c) (Partition ts) = Map.foldr m c subres
-    where subres = Map.map (eval (Fold f m c)) ts
-  eval (Join k1 k2 f) (Partition ts1, Partition ts2) = Partition $ crossJoin k1 k2 f ts1 ts2
+  eval (Fold f m c) (Partition ts) = pfold f m c ts
+  eval (Join k1 k2 f) (Partition ts1, Partition ts2) = Partition $ pjoin k1 k2 f ts1 ts2
   eval Localize (Partition ts) | Map.null ts = Partition ts
-                               | otherwise   = Partition localT
-      where (k:ks) = Map.keys ts
-            localT = foldr (\from m -> send from k m) ts ks
+                               | otherwise   = Partition $ plocalize ts
   eval Materialize dat = dat
 
-crossJoin :: (Ord k, Table t) => (a -> k) -> (b -> k) -> (a -> b -> c) -> Map.Map ID (t a) -> Map.Map ID (t b) -> Map.Map ID (t c)
-crossJoin k1 k2 f as bs = localJoins `Map.union` nonlocalJoins
+-- Fold locally on each machine and then merge the respective results.
+pfold :: Table t => (a -> b -> b) -> (b -> b -> b) -> b -> Map.Map ID (t a) -> b
+pfold f m c ts = Map.foldr m c subres
+    where subres = Map.map (eval (Fold f m c)) ts
+
+
+-- First, join the data that already resides in the same machine (`localJoins`).
+-- Second, copy data from all machines `as` to all other machines `bs` (`asCopied`).
+-- Third, join the data newly copied data (`nonlocalJoins`).
+pjoin :: (Ord k, Table t) => (a -> k) -> (b -> k) -> (a -> b -> c) -> Map.Map ID (t a) -> Map.Map ID (t b) -> Map.Map ID (t c)
+pjoin k1 k2 f as bs = localJoins `Map.union` nonlocalJoins
     where localJoins = Map.intersectionWith (curry $ eval (Join k1 k2 f)) as bs
           bkeys = Map.keys bs
-          asCopied = Map.foldrWithKey (\ak a res -> foldr (\bk -> sendVal a bk) res bkeys) Map.empty as
+          asCopied = Map.foldrWithKey (\ak a res -> foldr (\bk -> if ak /= bk then sendVal a bk else id) res bkeys) Map.empty as
           nonlocalJoins = Map.intersectionWith (curry $ eval (Join k1 k2 f)) asCopied bs
+
+-- Move data from all other machines `ks` to the first machine `k`.
+plocalize :: Table t => Map.Map ID (t a) -> Map.Map ID (t a)
+plocalize ts = foldr (\from m -> send from k m) ts ks
+    where (k:ks) = Map.keys ts
+
 
 send :: Table t => ID -> ID -> Map.Map ID (t a) -> Map.Map ID (t a)
 send from to ts = sendVal t to $ Map.delete from ts
@@ -88,3 +103,12 @@ send from to ts = sendVal t to $ Map.delete from ts
 
 sendVal :: Table t => t a -> ID -> Map.Map ID (t a) -> Map.Map ID (t a)
 sendVal t to ts = mapExtend to t (tconcat t) ts
+
+
+
+
+-- Utility functions
+
+mapExtend :: Ord k => k -> b -> (b -> b) -> Map.Map k b -> Map.Map k b
+mapExtend k noth just m = Map.alter (Just. maybe noth just) k m
+
